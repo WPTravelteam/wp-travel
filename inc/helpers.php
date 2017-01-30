2ccb38b0295af39130f7ac5eb2dc768cf6bafe57
Jagat <prajapati.jagat@wensolutions.com>
2017-01-30 17:33:01 +0545

    function for dropdown added

--- inc/helpers.php
+++ inc/helpers.php
@@ -12,3 +12,43 @@ function wp_traval_get_settings() {
 	$settings = get_option( 'wp_travel_settings' );
 	return $settings;
 }
+
+/**
+ * Return dropdown.
+ *
+ * @param  array $args Arguments for dropdown list.
+ * @return HTML  return dropdown list.
+ */
+function wp_traval_get_dropdown_list( $args = array() ) {
+
+	$currency_list = wp_traval_get_currency_list();
+
+	$default = array(
+		'id'		=> '',
+		'class'		=> '',
+		'name'		=> '',
+		'option'	=> '',
+		'options'	=> '',
+		'selected'	=> '',
+		);
+
+	$args = array_merge( $default, $args );
+
+	$dropdown = '';
+	if ( is_array( $currency_list )  && count( $currency_list ) > 0 ) {
+		$dropdown .= '<select name="' . $args['name'] . '" id="' . $args['id'] . '" class="' . $args['class'] . '" >';
+		if ( '' != $args['option'] ) {
+			$dropdown .= '<option value="" >' . $args['option'] . '</option>';
+		}
+
+		foreach ( $currency_list as $key => $currency ) {
+
+			$dropdown .= '<option value="' . $key . '" ' . selected( $args['selected'], $key, false ) . '  >' . $currency . '</option>';
+		}
+		$dropdown .= '</select>';
+
+	}
+
+	return $dropdown;
+}
+
