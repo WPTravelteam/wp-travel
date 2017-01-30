01e710c628a2be03bf37270f5ff41eff5f02dbe9
Jagat <prajapati.jagat@wensolutions.com>
2017-01-30 16:07:48 +0545

    use of wp_traval_get_settings function

--- inc/admin/class-admin-assets.php
+++ inc/admin/class-admin-assets.php
@@ -24,10 +24,16 @@ class WP_Travel_Admin_Assets {
 		wp_register_script( 'wp-travel-tabs', $this->assets_path . 'assets/js/wp-travel-tabs.js', array( 'jquery', 'jquery-ui-tabs' ), WP_TRAVEL_VERSION, 1 );
 		wp_enqueue_script( 'wp-travel-tabs' );
 		if ( 'itineraries' === $screen->id ) {
+			$settings = wp_traval_get_settings();
+
+			$api_key = '';
+			if ( isset( $settings['google_map_api_key'] ) ) {
+				$api_key = $settings['google_map_api_key'];
+			}
 			wp_enqueue_script( 'traval-door-script-2', $this->assets_path . 'assets/js/jquery.wptraveluploader.js', array( 'jquery' ), '1.0.0', true );
 			wp_register_script( 'traval-door-script', $this->assets_path . 'assets/js/wp-travel-back-end.js', array( 'jquery', 'jquery-ui-tabs', 'jquery-gmaps' ), '', 1 );
 			wp_register_script( 'jquery-datepicker-lib', $this->assets_path . 'assets/js/lib/datepicker/datepicker.min.js', array( 'jquery' ), '2.2.3', true );
-			wp_register_script( 'google-map-api', 'https://maps.google.com/maps/api/js?libraries=places&key=' . WP_TRAVEL_GOOGLE_MAP_API_KEY, array(), '', 1 );
+			wp_register_script( 'google-map-api', 'https://maps.google.com/maps/api/js?libraries=places&key=' . $api_key, array(), '', 1 );
 			wp_register_script( 'jquery-gmaps', $this->assets_path . 'assets/js/lib/gmaps/gmaps.min.js', array( 'jquery', 'google-map-api' ), '', 1 );
 			wp_register_script( 'jquery-datepicker-lib-eng', $this->assets_path . 'assets/js/lib/datepicker/i18n/datepicker.en.js', array( 'jquery' ), '', 1 );
 			wp_register_script( 'wp-travel-media-upload', $this->assets_path . 'assets/js/wp-travel-media-upload.js', array( 'jquery', 'plupload-handlers', 'jquery-ui-sortable', 'jquery-datepicker-lib', 'jquery-datepicker-lib-eng' ), '', 1 );
