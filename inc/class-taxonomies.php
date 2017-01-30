14a6425a9d2a643b5c4dd035e8d1165050013e77
Jagat <prajapati.jagat@wensolutions.com>
2017-01-30 17:39:23 +0545

    Location taxonomy removed

--- inc/class-taxonomies.php
+++ inc/class-taxonomies.php
@@ -1,52 +1,23 @@
 <?php
 class Wp_Travel_Taxonomies {
 	public static function init() {
-		self::register_locations();
 		self::register_itinerary_types();
-	}
-
-	public static function register_locations() {
-		// Add new taxonomy, make it hierarchical (like categories).
-		$labels = array(
-			'name'              => _x( 'Locations', 'taxonomy general name', 'textdomain' ),
-			'singular_name'     => _x( 'Location', 'taxonomy singular name', 'textdomain' ),
-			'search_items'      => __( 'Search Locations', 'textdomain' ),
-			'all_items'         => __( 'All Locations', 'textdomain' ),
-			'parent_item'       => __( 'Parent Location', 'textdomain' ),
-			'parent_item_colon' => __( 'Parent Location:', 'textdomain' ),
-			'edit_item'         => __( 'Edit Location', 'textdomain' ),
-			'update_item'       => __( 'Update Location', 'textdomain' ),
-			'add_new_item'      => __( 'Add New Location', 'textdomain' ),
-			'new_item_name'     => __( 'New Location Name', 'textdomain' ),
-			'menu_name'         => __( 'Location', 'textdomain' ),
-		);
-
-		$args = array(
-			'hierarchical'      => true,
-			'labels'            => $labels,
-			'show_ui'           => true,
-			'show_admin_column' => true,
-			'query_var'         => true,
-			'rewrite'           => array( 'slug' => 'location' ),
-		);
-
-		register_taxonomy( 'itinerary_locations', array( 'itineraries' ), $args );
 	}
 
 	public static function register_itinerary_types() {
 		// Add new taxonomy, make it hierarchical (like categories).
 		$labels = array(
-			'name'              => _x( 'Trip Types', 'taxonomy general name', 'textdomain' ),
-			'singular_name'     => _x( 'Trip Type', 'taxonomy singular name', 'textdomain' ),
-			'search_items'      => __( 'Search Trip Types', 'textdomain' ),
-			'all_items'         => __( 'All Trip Types', 'textdomain' ),
-			'parent_item'       => __( 'Parent Trip Type', 'textdomain' ),
-			'parent_item_colon' => __( 'Parent Trip Type:', 'textdomain' ),
-			'edit_item'         => __( 'Edit Trip Type', 'textdomain' ),
-			'update_item'       => __( 'Update Trip Type', 'textdomain' ),
-			'add_new_item'      => __( 'Add New Trip Type', 'textdomain' ),
-			'new_item_name'     => __( 'New Tour Trip Name', 'textdomain' ),
-			'menu_name'         => __( 'Trip Type', 'textdomain' ),
+			'name'              => _x( 'Trip Types', 'taxonomy general name', 'wp-travel' ),
+			'singular_name'     => _x( 'Trip Type', 'taxonomy singular name', 'wp-travel' ),
+			'search_items'      => __( 'Search Trip Types', 'wp-travel' ),
+			'all_items'         => __( 'All Trip Types', 'wp-travel' ),
+			'parent_item'       => __( 'Parent Trip Type', 'wp-travel' ),
+			'parent_item_colon' => __( 'Parent Trip Type:', 'wp-travel' ),
+			'edit_item'         => __( 'Edit Trip Type', 'wp-travel' ),
+			'update_item'       => __( 'Update Trip Type', 'wp-travel' ),
+			'add_new_item'      => __( 'Add New Trip Type', 'wp-travel' ),
+			'new_item_name'     => __( 'New Tour Trip Name', 'wp-travel' ),
+			'menu_name'         => __( 'Trip Type', 'wp-travel' ),
 		);
 
 		$args = array(
