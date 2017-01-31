1cfdf81d50ef98300308844b15ad757d08a53df6
Jagat <prajapati.jagat@wensolutions.com>
2017-01-31 14:09:08 +0545

    map marker centered on initial load

--- assets/js/wp-travel-back-end.js
+++ assets/js/wp-travel-back-end.js
@@ -92,20 +92,21 @@
    		* Tab js.
    		*/
    	if ($.fn.tabs) {
-   					$('.wp-travel-tabs-wrap').tabs({
-   									activate: function (event, ui) {
-   													$(ui.newPanel).css({display: 'table'});
-   													$('#wp-travel-settings-current-tab').val( $(ui.newPanel).attr('id') );
-                map.refresh();
-   									},
-   									create: function (event, ui) {
-   													$(ui.panel).css({display: 'table'});
-   													$('#wp-travel-settings-current-tab').val( $(ui.panel).attr('id') );
-   									},
-   									load: function( event, ui ) {
-   										// console.log( ui );
-   									}
-   					});
+		$('.wp-travel-tabs-wrap').tabs({
+			activate: function (event, ui) {
+							$(ui.newPanel).css({display: 'table'});
+							$('#wp-travel-settings-current-tab').val( $(ui.newPanel).attr('id') );
+							map.refresh();
+							map.setCenter(wp_travel_drag_drop_uploader.lat, wp_travel_drag_drop_uploader.lng);
+						},
+						create: function (event, ui) {
+							$(ui.panel).css({display: 'table'});
+							$('#wp-travel-settings-current-tab').val( $(ui.panel).attr('id') );
+						},
+						load: function( event, ui ) {
+				// console.log( ui );
+			}
+		});
    	}
 
     if ($.fn.datepicker) {
