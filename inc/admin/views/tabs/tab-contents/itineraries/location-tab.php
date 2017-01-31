b370e1c96d9fc517f9e1c31bef71e13a06b3b7a3
Jagat <prajapati.jagat@wensolutions.com>
2017-01-31 13:57:38 +0545

    hidden fields for lat lng and location added along with its value

--- inc/admin/views/tabs/tab-contents/itineraries/location-tab.php
+++ inc/admin/views/tabs/tab-contents/itineraries/location-tab.php
@@ -1,6 +1,30 @@
+<?php
+global $post;
+
+$post_meta_lat = get_post_meta( $post->ID, 'wp_traval_lat', true );
+$post_meta_lng = get_post_meta( $post->ID, 'wp_traval_lng', true );
+$post_meta_loc = get_post_meta( $post->ID, 'wp_traval_location', true );
+
+$lat = -12.043333;
+if ( isset( $post_meta_lat ) && '' != $post_meta_lat ) {
+	$lat = $post_meta_lat;
+}
+
+$lng = -77.028333;
+if ( isset( $post_meta_lng ) && '' != $post_meta_lng ) {
+	$lng = $post_meta_lng;
+}
+
+$loc = __( 'Lima' );
+if ( isset( $post_meta_loc ) && '' != $post_meta_loc ) {
+	$loc = $post_meta_loc;
+} ?>
 <div class="map-wrap">
-  <input id="search-input" class="controls" type="text" placeholder="Enter a location">
+  <input id="search-input" class="controls" type="text" placeholder="Enter a location" value="<?php echo esc_html( $loc ); ?>" >
   <div id="gmap" style="width:100%;height:300px"></div>
+  <input type="hidden" name="wp_traval_location" id="wp-traval-location" value="<?php echo esc_html( $lat ); ?>" >
+  <input type="hidden" name="wp_traval_lat" id="wp-traval-lat" value="<?php echo esc_html( $lng ); ?>" >
+  <input type="hidden" name="wp_traval_lng" id="wp-traval-lng" value="<?php echo esc_html( $loc ); ?>" >
 </div>
 <style>
 .map-wrap{
