24104bc26cb14c90564c2129f2755ea576cb541d
Jagat <prajapati.jagat@wensolutions.com>
2017-02-02 12:52:05 +0545

    location dropdown added as per location post type

--- inc/admin/views/tabs/tab-contents/itineraries/location-tab.php
+++ inc/admin/views/tabs/tab-contents/itineraries/location-tab.php
@@ -1,6 +1,20 @@
 <?php
 global $post;
 
+
+$paged = get_query_var('paged');
+$args = array(
+	'numberposts'       => -1,
+	'offset'            => $paged*4,
+	'orderby'           => 'post_title',
+	'order'             => 'ASC',
+	'post_type'         => 'travel_locations',
+	'post_status'       => 'publish',
+	'suppress_filters'  => true
+	);
+$posts_array = get_posts( $args );
+$hierarchical_post = wp_traval_build_post_tree( $posts_array );
+
 $post_meta_lat = get_post_meta( $post->ID, 'wp_traval_lat', true );
 $post_meta_lng = get_post_meta( $post->ID, 'wp_traval_lng', true );
 $post_meta_loc = get_post_meta( $post->ID, 'wp_traval_location', true );
@@ -19,12 +33,30 @@ $loc = __( 'Lima' );
 if ( isset( $post_meta_loc ) && '' != $post_meta_loc ) {
 	$loc = $post_meta_loc;
 } ?>
+<div class="location-wrap">
+	<label id="wp-traval-locations"><?php esc_html_e( 'Locations' ); ?></label>
+	<?php if ( is_array( $hierarchical_post ) && count( $hierarchical_post ) > 0 ) : ?>
+	<select name="wp_traval_location_id">
+		<option value=""><?php esc_html_e( 'Select Locations' ); ?></option>
+		<?php
+		$location_id = ( int ) get_post_meta( $post->ID, 'wp_traval_location_id', true );
+
+		$selected = '';
+		if ( $location_id ) {
+			$selected = $location_id;
+		}
+		wp_traval_get_post_hierarchy_dropdown( $hierarchical_post, $selected );
+		wp_reset_postdata(); ?>
+	</select>
+	<?php endif; ?>
+</div>
+
 <div class="map-wrap">
   <input id="search-input" class="controls" type="text" placeholder="Enter a location" value="<?php echo esc_html( $loc ); ?>" >
   <div id="gmap" style="width:100%;height:300px"></div>
-  <input type="hidden" name="wp_traval_location" id="wp-traval-location" value="<?php echo esc_html( $lat ); ?>" >
-  <input type="hidden" name="wp_traval_lat" id="wp-traval-lat" value="<?php echo esc_html( $lng ); ?>" >
-  <input type="hidden" name="wp_traval_lng" id="wp-traval-lng" value="<?php echo esc_html( $loc ); ?>" >
+  <input type="hidden" name="wp_traval_location" id="wp-traval-location" value="<?php echo esc_html( $loc ); ?>" >
+  <input type="hidden" name="wp_traval_lat" id="wp-traval-lat" value="<?php echo esc_html( $lat ); ?>" >
+  <input type="hidden" name="wp_traval_lng" id="wp-traval-lng" value="<?php echo esc_html( $lng ); ?>" >
 </div>
 <style>
 .map-wrap{
