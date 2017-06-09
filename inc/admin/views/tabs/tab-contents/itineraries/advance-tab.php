960fbd11ab4bafc0fdbc5f0d5ed477070a2e6091
Racase Lawaju <codearryaas@gmail.com>
2017-06-09 17:39:28 +0545

    wrapper class added

--- inc/admin/views/tabs/tab-contents/itineraries/advance-tab.php
+++ inc/admin/views/tabs/tab-contents/itineraries/advance-tab.php
@@ -3,7 +3,7 @@
 global $post;
 $obj = get_post_type_object( 'itineraries' );
 
-echo '<div class="trip-type-wrap">';
+echo '<div class="trip-type-wrap itineraries-tax-wrap">';
 post_categories_meta_box( $post, array( 'args' => array( 'taxonomy' => 'itinerary_types' ) ) );
 printf( '<div class="tax-edit"><a href="' . esc_url( admin_url( 'edit-tags.php?taxonomy=itinerary_types&post_type=itineraries' ) ) . '">%s</a></div>', esc_html__( 'Edit All Trip Type' ) );
 echo '</div>';
