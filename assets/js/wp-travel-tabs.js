f7624548f22dee0951b8f9d4817e6b04ca338de1
Racase Lawaju <codearryaas@gmail.com>
2017-06-12 17:35:21 +0545

    added tabs script for settings

--- assets/js/wp-travel-tabs.js
+++ assets/js/wp-travel-tabs.js
@@ -1,3 +1,19 @@
 (function ($) {
-	
+	if ($.fn.tabs) {
+		$('.wp-travel-tabs-wrap').tabs({
+			activate: function (event, ui) {
+							$(ui.newPanel).css({display: 'table'});
+							$('#wp-travel-settings-current-tab').val( $(ui.newPanel).attr('id') );
+							map.refresh();
+							map.setCenter( wp_travel_drag_drop_uploader.lat, wp_travel_drag_drop_uploader.lng );
+						},
+						create: function (event, ui) {
+							$(ui.panel).css({display: 'table'});
+							$('#wp-travel-settings-current-tab').val( $(ui.panel).attr('id') );
+						},
+						load: function( event, ui ) {
+				// console.log( ui );
+			}
+		});
+   	}
 }(jQuery));
