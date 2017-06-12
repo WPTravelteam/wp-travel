c08d56be080bbdd5286f8c354e5445c8cb4aad72
Racase Lawaju <codearryaas@gmail.com>
2017-06-12 17:38:25 +0545

    removed location post type and added location taxonomy

--- inc/admin/views/tabs/tab-contents/itineraries/location-tab.php
+++ inc/admin/views/tabs/tab-contents/itineraries/location-tab.php
@@ -1,62 +1,23 @@
 <?php
 global $post;
 
+$map_data = get_wp_travel_map_data();
 
-$paged = get_query_var('paged');
-$args = array(
-	'numberposts'       => -1,
-	'offset'            => $paged*4,
-	'orderby'           => 'post_title',
-	'order'             => 'ASC',
-	'post_type'         => 'travel_locations',
-	'post_status'       => 'publish',
-	'suppress_filters'  => true
-	);
-$posts_array = get_posts( $args );
-$hierarchical_post = wp_traval_build_post_tree( $posts_array );
 
-$post_meta_lat = get_post_meta( $post->ID, 'wp_traval_lat', true );
-$post_meta_lng = get_post_meta( $post->ID, 'wp_traval_lng', true );
-$post_meta_loc = get_post_meta( $post->ID, 'wp_traval_location', true );
-
-$lat = -12.043333;
-if ( isset( $post_meta_lat ) && '' != $post_meta_lat ) {
-	$lat = $post_meta_lat;
-}
-
-$lng = -77.028333;
-if ( isset( $post_meta_lng ) && '' != $post_meta_lng ) {
-	$lng = $post_meta_lng;
-}
-
-$loc = __( 'Lima' );
-if ( isset( $post_meta_loc ) && '' != $post_meta_loc ) {
-	$loc = $post_meta_loc;
-} ?>
-<div class="location-wrap">
-	<label id="wp-traval-locations"><?php esc_html_e( 'Map' ); ?></label>
-	<?php if ( is_array( $hierarchical_post ) && count( $hierarchical_post ) > 0 ) : ?>
-	<select name="wp_traval_location_id">
-		<option value=""><?php esc_html_e( 'Select Locations' ); ?></option>
-		<?php
-		$location_id = ( int ) get_post_meta( $post->ID, 'wp_traval_location_id', true );
-
-		$selected = '';
-		if ( $location_id ) {
-			$selected = $location_id;
-		}
-		wp_traval_get_post_hierarchy_dropdown( $hierarchical_post, $selected );
-		wp_reset_postdata(); ?>
-	</select>
-	<?php endif; ?>
+?>
+<div class="location-wrap itineraries-tax-wrap">
+  <?php
+  post_categories_meta_box( $post, array( 'args' => array( 'taxonomy' => 'travel_locations' ) ) );
+  printf( '<div class="tax-edit"><a href="' . esc_url( admin_url( 'edit-tags.php?taxonomy=travel_locations&post_type=itineraries' ) ) . '">%s</a></div>', esc_html__( 'Edit All Locations' ) );
+  ?>
 </div>
 
 <div class="map-wrap">
-  <input id="search-input" class="controls" type="text" placeholder="Enter a location" value="<?php echo esc_html( $loc ); ?>" >
+  <input id="search-input" class="controls" type="text" placeholder="Enter a location" value="<?php echo esc_html( $map_data['loc'] ); ?>" >
   <div id="gmap" style="width:100%;height:300px"></div>
-  <input type="hidden" name="wp_traval_location" id="wp-traval-location" value="<?php echo esc_html( $loc ); ?>" >
-  <input type="hidden" name="wp_traval_lat" id="wp-traval-lat" value="<?php echo esc_html( $lat ); ?>" >
-  <input type="hidden" name="wp_traval_lng" id="wp-traval-lng" value="<?php echo esc_html( $lng ); ?>" >
+  <input type="hidden" name="wp_traval_location" id="wp-traval-location" value="<?php echo esc_html( $map_data['loc'] ); ?>" >
+  <input type="hidden" name="wp_traval_lat" id="wp-traval-lat" value="<?php echo esc_html( $map_data['lat'] ); ?>" >
+  <input type="hidden" name="wp_traval_lng" id="wp-traval-lng" value="<?php echo esc_html( $map_data['lng'] ); ?>" >
 </div>
 <style>
 .map-wrap{
