e6e14685a6f07d8cbb4ab4c2eaa0fdd309a6eecf
Racase Lawaju <codearryaas@gmail.com>
2017-06-14 18:50:57 +0545

    booking post type added

--- inc/class-post-types.php
+++ inc/class-post-types.php
@@ -7,7 +7,7 @@ class WP_Travel_Post_Types {
 
 	public static function init() {
 		self::register_itineraries();
-		// self::register_locations();
+		self::register_bookings();
 	}
 
 	public static function register_itineraries() {
@@ -52,45 +52,45 @@ class WP_Travel_Post_Types {
 		register_post_type( 'itineraries', $args );
 	}
 
-	// public static function register_locations() {
-	// 	$labels = array(
-	// 		'name'               => _x( 'Locations', 'post type general name', 'wp-travel' ),
-	// 		'singular_name'      => _x( 'Location', 'post type singular name', 'wp-travel' ),
-	// 		'menu_name'          => _x( 'Locations', 'admin menu', 'wp-travel' ),
-	// 		'name_admin_bar'     => _x( 'Location', 'add new on admin bar', 'wp-travel' ),
-	// 		'add_new'            => _x( 'Add New', 'wp-travel', 'wp-travel' ),
-	// 		'add_new_item'       => __( 'Add New location', 'wp-travel' ),
-	// 		'new_item'           => __( 'New location', 'wp-travel' ),
-	// 		'edit_item'          => __( 'Edit location', 'wp-travel' ),
-	// 		'view_item'          => __( 'View location', 'wp-travel' ),
-	// 		'all_items'          => __( 'Locations', 'wp-travel' ),
-	// 		'search_items'       => __( 'Search Locations', 'wp-travel' ),
-	// 		'parent_item_colon'  => __( 'Parent Locations:', 'wp-travel' ),
-	// 		'not_found'          => __( 'No Locations found.', 'wp-travel' ),
-	// 		'not_found_in_trash' => __( 'No Locations found in Trash.', 'wp-travel' ),
-	// 	);
+	public static function register_bookings() {
+		$labels = array(
+			'name'               => _x( 'Bookings', 'post type general name', 'wp-travel' ),
+			'singular_name'      => _x( 'Booking', 'post type singular name', 'wp-travel' ),
+			'menu_name'          => _x( 'Bookings', 'admin menu', 'wp-travel' ),
+			'name_admin_bar'     => _x( 'Booking', 'add new on admin bar', 'wp-travel' ),
+			'add_new'            => _x( 'Add New', 'wp-travel', 'wp-travel' ),
+			'add_new_item'       => __( 'Add New booking', 'wp-travel' ),
+			'new_item'           => __( 'New booking', 'wp-travel' ),
+			'edit_item'          => __( 'Edit booking', 'wp-travel' ),
+			'view_item'          => __( 'View booking', 'wp-travel' ),
+			'all_items'          => __( 'Bookings', 'wp-travel' ),
+			'search_items'       => __( 'Search bookings', 'wp-travel' ),
+			'parent_item_colon'  => __( 'Parent bookings:', 'wp-travel' ),
+			'not_found'          => __( 'No bookings found.', 'wp-travel' ),
+			'not_found_in_trash' => __( 'No bookings found in Trash.', 'wp-travel' ),
+		);
 
-	// 	$args = array(
-	// 		'labels'             => $labels,
-	// 		'description'        => __( 'Description.', 'wp-travel' ),
-	// 		'public'             => true,
-	// 		'publicly_queryable' => true,
-	// 		'show_ui'            => true,
-	// 		'show_in_menu'       => 'edit.php?post_type=itineraries',
-	// 		'query_var'          => true,
-	// 		'rewrite'            => array( 'slug' => 'location' ),
-	// 		'capability_type'    => 'post',
-	// 		'has_archive'        => true,
-	// 		'hierarchical'       => true,
-	// 		'menu_position'      => null,
-	// 		'supports'           => array( 'title', 'page-attributes' ),
-	// 		'menu_icon'          => 'dashicons-location',
-	// 	);
-	// 	/**
-	// 	 * Register a travaldoor_trip post type.
-	// 	 *
-	// 	 * @link http://codex.wordpress.org/Function_Reference/register_post_type
-	// 	 */
-	// 	// register_post_type( 'travel_locations', $args );
-	// }
+		$args = array(
+			'labels'             => $labels,
+			'description'        => __( 'Description.', 'wp-travel' ),
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => 'edit.php?post_type=itineraries',
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'itinerary-booking' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => true,
+			'menu_position'      => null,
+			'supports'           => array( 'title', 'page-attributes', 'editor' ),
+			'menu_icon'          => 'dashicons-location',
+		);
+		/**
+		 * Register a travaldoor_trip post type.
+		 *
+		 * @link http://codex.wordpress.org/Function_Reference/register_post_type
+		 */
+		register_post_type( 'itinerary-booking', $args );
+	}
 }
