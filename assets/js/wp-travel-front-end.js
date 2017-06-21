6663663836738efefe4660e76308857d3534aeb3
Racase Lawaju <codearryaas@gmail.com>
2017-06-21 09:33:39 +0545

    frontend script updated

--- assets/js/wp-travel-front-end.js
+++ assets/js/wp-travel-front-end.js
@@ -1,17 +1,19 @@
 jQuery( document ).ready( function($){
 
-	var base_height = 0;
-	$( '.wp-travel-feature-slide-content' ).each( function(){
-		// alert( $( this ).height() );
-		if ( $( this ).height() > base_height ) {			
-			base_height = $( this ).height();
+	function wp_travel_set_equal_height() {
+		var base_height = 0;
+		$('.wp-travel-feature-slide-content').css({'height':'auto'});
+		$( '.wp-travel-feature-slide-content' ).each( function(){
+			if ( $( this ).height() > base_height ) {
+				base_height = $( this ).height();
+			}
+		} );
+		if( base_height > 0 ) {
+			$( '.trip-headline-wrapper .left-plot' ).height( base_height ); // Adding Padding of right plot.
+			$( '.trip-headline-wrapper .right-plot' ).height( base_height ); 
 		}
-	} );
-	if( base_height > 0 ) {
-		$( '.trip-headline-wrapper .left-plot' ).height( base_height );
-		$( '.trip-headline-wrapper .right-plot' ).height( base_height-50 ); // reducing padding
 	}
-
+	wp_travel_set_equal_height();
 	// Create map.
     var map = new GMaps({
 	        div: '#gmap',
@@ -42,16 +44,29 @@ jQuery( document ).ready( function($){
 	    enabled:true
 	  }
 	});
-	$('ul.tab-list li').click(function(){
-		var tab_id = $(this).attr('data-tab');
+	// $('ul.tab-list li').click(function(){
+	// 	var tab_id = $(this).attr('data-tab');
 
-		$('ul.tab-list li').removeClass('current-content');
-		$('.tab-list-content').removeClass('current-content');
+	// 	$('ul.tab-list li').removeClass('current-content');
+	// 	$('.tab-list-content').removeClass('current-content');
 
-		$(this).addClass('current-content');
-		$("#"+tab_id).addClass('current-content');
+	// 	$(this).addClass('current-content');
+	// 	$("#"+tab_id).addClass('current-content');
+	// });
+
+	$('#wp-travel-tab-wrapper').easyResponsiveTabs({
+		type: 'default', //Types: default, vertical, accordion           
+		width: 'auto', //auto or any width like 600px
+		fit: true,   // 100% fit in a container
+		closed: 'accordion', // Start closed if in accordion view
+		activate: function(event) { // Callback function if tab is switched
+		var $tab = $(this);
+		var $info = $('#tabInfo');
+		var $name = $('span', $info);
+		$name.text($tab.text());
+		$info.show();
+		}
 	});
-
 	// Rating script starts.
 	$( '.rate_label' ).hover( function(){
 		var rateLabel = $( this ).attr('data-id');
@@ -84,8 +99,49 @@ jQuery( document ).ready( function($){
 		e.preventDefault();
 		$('#wp_travel_rate_val').val( $( this ).attr( 'data-id' ) );
 	});
-
-
 	// Rating script ends.
+	// 
+	// 
+	$(document).on( 'click', '.wp-travel-count-info', function() {		
+		$( ".wp-travel-review" ).trigger( "click" );
+	} );
+
+	// Scroll on click
+	$(document).on( 'click', '.wp-travel-count-info, .wp-travel-booknow-btn', function() {		
+		
+		var tabHeight = $('.wp-travel-tab-wrapper').offset().top; 
+		$('html, body').animate({
+		      scrollTop: ( tabHeight )
+		    }, 1200 );
+	} );
+
+
+
+	// Scroll and resize event
+	$(window).on("scroll",function(e){
+		
+		var tabWrapper 	= $('.wp-travel-tab-wrapper');
+		var tabMenu 	= $( 'ul.wp-travel.tab-list' );
+
+		var tabWrapperTopOffset = tabWrapper.offset().top;
+		var tabWrapperHeight = tabWrapper.height();
+		var tabMenuHeight	 = tabMenu.height();
+
+		var winScrollTop 	= $( window ).scrollTop();
+		if ( winScrollTop < ( tabWrapperTopOffset + tabWrapperHeight ) &&  winScrollTop >= tabWrapperTopOffset ) {
+			tabMenu.addClass( 'fixed' );
+			tabWrapper.css( {'padding-top': tabMenuHeight} );
+		} else {
+			tabMenu.removeClass( 'fixed' );
+			tabWrapper.css( {'padding-top': 0} );
+		}
+		
+	});
+	$(window).on("resize",function(e){
+		wp_travel_set_equal_height();
+		console.log( 'resized' );
+	});
+
+
 
 } );
