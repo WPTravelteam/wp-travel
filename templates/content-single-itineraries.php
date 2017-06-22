090fe82b19602e2e4515932a04fb90d592806000
Racase Lawaju <codearryaas@gmail.com>
2017-06-22 17:43:40 +0545

    fetched thumbnail by wp travel thumbnail function

--- templates/content-single-itineraries.php
+++ templates/content-single-itineraries.php
@@ -10,6 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	 	echo get_the_password_form();
 	 	return;
 	 }
+	 $enable_sale 	= get_post_meta( get_the_ID(), 'wp_travel_enable_sale', true );
 ?>
 
 <div id="itinerary-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -17,16 +18,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<div class="content entry-content">
 		<div class="wp-travel trip-headline-wrapper">
 	         <div class="wp-travel-feature-slide-content featured-side-image left-plot">
-	           <div class="banner-image-wrapper" style="background:url(<?php echo  get_the_post_thumbnail_url(); ?>)">
-	         	   <!--  <?php the_post_thumbnail(); ?> -->
-	         	</div>
+	            <div class="banner-image-wrapper" style="background-image: url(<?php echo wp_travel_get_post_thumbnail_url( get_the_ID() ) ?>)">
+	         	  <?php //the_post_thumbnail(); ?>
+
+	         	  <?php 
+	         	  echo wp_travel_get_post_thumbnail( get_the_ID() ); 
+	         	  ?>
+	         	</div> 
+	         	<?php if ( $enable_sale ) : ?>     	
+      			<div class="wp-travel-offer">
+      			    <span><?php esc_html_e( 'Offer', 'wp-travel' ) ?></span>
+      			</div>
+      			<?php endif; ?>
+      			<div class="wp-travel-view-gallery">
+      				<a class="top-view-gallery" href=""><?php esc_html_e( 'View Gallery', 'wp-travel' ) ?></a>
+      			</div>
 	         </div>
 	         <div class="wp-travel-feature-slide-content featured-detail-section right-plot">
 	           <div class="right-plot-inner-wrap">
 		         	<?php do_action( 'wp_tarvel_before_single_title', get_the_ID() ) ?>
+		         	<?php $show_title = apply_filters( 'wp_travel_show_single_page_title', true ); ?>
+		         	<?php if ( $show_title ) : ?>
 		         	<header class="entry-header">
 					<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
 					</header>
+					<?php endif; ?>
 					<?php do_action( 'wp_tarvel_after_single_title', get_the_ID() ) ?>
 				</div>
 	        </div>
