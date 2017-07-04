dc268e17157f295380258276525b2183fb3d94dc
Racase Lawaju <codearryaas@gmail.com>
2017-07-04 13:01:43 +0545

    hook name changed and wrapper class removed

--- templates/archive-itineraries.php
+++ templates/archive-itineraries.php
@@ -1,10 +1,16 @@
 <?php get_header( 'itinerary' ); ?>
+<div class="">
 <?php do_action( 'wp_travel_before_main_content' ); ?>
-<div class="container">
 <?php while ( have_posts() ) : the_post(); ?>
 
 	<?php wp_travel_get_template_part( 'content', 'archive-itineraries' ); ?>
 
-<?php endwhile; // end of the loop. ?></div>
-<?php do_action( 'wp_travel_before_main_content' ); ?>
+<?php endwhile; // end of the loop. ?>	
+<?php do_action( 'wp_travel_after_main_content' ); ?>
+</div>
+<?php
+$pagination_range = apply_filters( 'wp_travel_pagination_range', 2 );
+$max_num_pages    = apply_filters( 'wp_travel_max_num_pages', '' );
+?>
+<?php wp_travel_pagination( $pagination_range, $max_num_pages ); ?>
 <?php get_footer( 'itinerary' ); ?>
