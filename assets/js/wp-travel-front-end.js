caa84344abb0fd62920e9e2293cdc3244555c71b
Racase Lawaju <codearryaas@gmail.com>
2017-07-11 10:24:53 +0545

    map script removed from frontend scripts

--- assets/js/wp-travel-front-end.js
+++ assets/js/wp-travel-front-end.js
@@ -17,28 +17,7 @@ jQuery( document ).ready( function($){
 			}
 		}
 	}
-	wp_travel_set_equal_height();
-	// Create map.
-    var map = new GMaps({
-	        div: '#gmap',
-	        lat: wp_travel.lat,
-	        lng: wp_travel.lng,
-	        scrollwheel: false,
-		    navigationControl: false,
-		    mapTypeControl: false,
-	        scaleControl: false,
-    		// draggable: false,
-	    });
-
-    map.setCenter( wp_travel.lat, wp_travel.lng );
-    map.setZoom( 15 );
-    map.addMarker({
-        lat: wp_travel.lat,
-        lng: wp_travel.lng,
-        title: wp_travel.loc,
-        draggable: false
-        
-    });
+	wp_travel_set_equal_height();	
 
     $('.wp-travel-gallery').magnificPopup({
 	  delegate: 'a', // child items selector, by clicking on it popup will open
