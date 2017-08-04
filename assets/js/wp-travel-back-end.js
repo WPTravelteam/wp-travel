3b2b00a5acbbd904808c54c3f6d733f80b6f75ec
Racase Lawaju <codearryaas@gmail.com>
2017-08-04 18:20:53 +0545

    changing map js

--- assets/js/wp-travel-back-end.js
+++ assets/js/wp-travel-back-end.js
@@ -95,6 +95,24 @@ if ( 'undefined' !=  typeof( GMaps ) ) {
 
 	});
 }
+
+if ( 'undefined' !=  typeof( Maplace ) ) {
+	var lat = wp_travel_drag_drop_uploader.lat;
+	var lon = wp_travel_drag_drop_uploader.lng;
+    var map = new Maplace({
+			    map_options: {
+					set_center: [lat, lon],
+					zoom: 16,
+					visualRefresh:true,
+				},
+				controls_on_map: false
+			});
+    input = document.getElementById('search-input');
+	autocomplete = new google.maps.places.Autocomplete(input);
+    
+}
+
+
     /*
    		* Tab js.
    		*/
@@ -103,8 +121,79 @@ if ( 'undefined' !=  typeof( GMaps ) ) {
 			activate: function (event, ui) {
 							$(ui.newPanel).css({display: 'table'});
 							$('#wp-travel-settings-current-tab').val( $(ui.newPanel).attr('id') );
-							map.refresh();
-							map.setCenter( wp_travel_drag_drop_uploader.lat, wp_travel_drag_drop_uploader.lng );
+							if ( map ) {
+
+								var lat = wp_travel_drag_drop_uploader.lat;
+								var lon = wp_travel_drag_drop_uploader.lng;
+								map.Load({
+						            locations: [{
+												lat: lat,
+						        				lon: lon,
+												zoom: 16,
+												set_center: [lat, lon],
+										    }],
+						            force_generate_controls: true
+						        });
+
+								// autocomplete.bindTo('bounds', map);
+						        autocomplete.addListener( 'place_changed', function() {
+						        	// $('#gmap').html('');
+									var place = autocomplete.getPlace();		
+									if ( ! place.geometry ) {
+										window.alert( "Autocomplete's returned place contains no geometry" );
+										return;
+									}
+									
+									// If the place has a geometry, then present it on a map.
+									if ( place.geometry.viewport ) {
+										// map.fitBounds( place.geometry.viewport );
+									} else {
+										map.setCenter( place.geometry.location );
+										map.setZoom(15);
+									}
+									var lat = place.geometry.location.lat();
+									var lng = place.geometry.location.lng();
+
+									var latlng = new google.maps.LatLng(lat, lng);
+							        var geocoder = geocoder = new google.maps.Geocoder();
+							        geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+							            if (status == google.maps.GeocoderStatus.OK) {
+							                if (results[1]) {
+							                    $( '#wp-traval-lat' ).val( lat );
+							                	$( '#wp-traval-lng' ).val( lng );
+							                	$( '#wp-traval-location' ).val( results[1].formatted_address );
+							                	$( '#search-input' ).val( results[1].formatted_address );
+							                }
+							            }
+							        });
+							        map.Load({
+							            locations: [{
+													lat: lat,
+							        				lon: lng,
+													zoom: 16,
+													set_center: [lat, lng],
+													visualRefresh:true
+											    }],
+							            force_generate_controls: true
+							        });
+
+								});
+
+    							// autocomplete = new google.maps.places.Autocomplete(input);
+
+								// autocomplete.bindTo( 'bounds', map );
+
+								// autocomplete.addListener( 'place_changed', function() {
+								// 	var place = autocomplete.getPlace();		
+								// 	console.log( place );
+								// 	map.RemoveLocations();
+								// });  
+								
+								// // refresh();
+								// google.maps.event.trigger(new google.maps.Map('#gmap', ''), 'resize');
+								// map.refresh();
+								// map.setCenter( wp_travel_drag_drop_uploader.lat, wp_travel_drag_drop_uploader.lng );
+							}
 						},
 						create: function (event, ui) {
 							$(ui.panel).css({display: 'table'});
