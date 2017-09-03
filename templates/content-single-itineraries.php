417056fd961a6aac1823f31b2baf6dee2232bc86
Rakesh Lawaju <codearryaas@gmail.com>
2017-09-03 15:44:03 +0545

    Code purity

--- templates/content-single-itineraries.php
+++ templates/content-single-itineraries.php
@@ -1,38 +1,40 @@
 <?php
+/**
+ * Itinerary single content.
+ *
+ * @package WP Travel
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 ?>
 
 <?php
-	 do_action( 'wp_travel_before_single_itinerary', get_the_ID() );
-	 if ( post_password_required() ) {
-	 	echo get_the_password_form();
-	 	return;
-	 }
-	 $enable_sale 	= get_post_meta( get_the_ID(), 'wp_travel_enable_sale', true );
+do_action( 'wp_travel_before_single_itinerary', get_the_ID() );
+if ( post_password_required() ) {
+	echo get_the_password_form();
+	return;
+}
+$enable_sale 	= get_post_meta( get_the_ID(), 'wp_travel_enable_sale', true );
 ?>
 
 <div id="itinerary-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
+
 	<div class="content entry-content">
 		<div class="wp-travel trip-headline-wrapper">
 	         <div class="wp-travel-feature-slide-content featured-side-image left-plot">
-	            <div class="banner-image-wrapper" style="background-image: url(<?php echo wp_travel_get_post_thumbnail_url( get_the_ID() ) ?>)">
-	         	  <?php //the_post_thumbnail(); ?>
-
-	         	  <?php 
-	         	  echo wp_travel_get_post_thumbnail( get_the_ID() ); 
-	         	  ?>
-	         	</div> 
-	         	<?php if ( $enable_sale ) : ?>     	
-      			<div class="wp-travel-offer">
-      			    <span><?php esc_html_e( 'Offer', 'wp-travel' ) ?></span>
-      			</div>
-      			<?php endif; ?>
-      			<div class="wp-travel-view-gallery">
-      				<a class="top-view-gallery" href=""><?php esc_html_e( 'View Gallery', 'wp-travel' ) ?></a>
-      			</div>
+	            <div class="banner-image-wrapper" style="background-image: url(<?php echo esc_url( wp_travel_get_post_thumbnail_url( get_the_ID() ) ) ?>)">
+						<?php echo wp_kses( wp_travel_get_post_thumbnail( get_the_ID() ) ); ?>
+	         	</div>
+	         	<?php if ( $enable_sale ) : ?>
+	      			<div class="wp-travel-offer">
+	      			    <span><?php esc_html_e( 'Offer', 'wp-travel' ) ?></span>
+	      			</div>
+	      			<?php endif; ?>
+	      			<div class="wp-travel-view-gallery">
+	      				<a class="top-view-gallery" href=""><?php esc_html_e( 'View Gallery', 'wp-travel' ) ?></a>
+	      			</div>
 	         </div>
 	         <div class="wp-travel-feature-slide-content featured-detail-section right-plot">
 	           <div class="right-plot-inner-wrap">
@@ -40,17 +42,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 		         	<?php $show_title = apply_filters( 'wp_travel_show_single_page_title', true ); ?>
 		         	<?php if ( $show_title ) : ?>
 		         	<header class="entry-header">
-					<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-					</header>
+							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+						</header>
 					<?php endif; ?>
 					<?php do_action( 'wp_tarvel_after_single_title', get_the_ID() ) ?>
 				</div>
 	        </div>
 	    </div>
 	    <?php do_action( 'wp_travel_after_single_itinerary_header', get_the_ID() ); ?>
-        <?php //do_action( 'wp_travel_before_main_content' ); ?>
-		<?php //echo apply_filters( 'the_content', get_the_content() ); ?>
-		<?php //do_action( 'wp_travel_after_main_content' ); ?>
 	</div><!-- .summary -->
 
 </div><!-- #itinerary-<?php the_ID(); ?> -->
