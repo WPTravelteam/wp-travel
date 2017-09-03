e8eed31719d26c3ab98c40ec8a2b05883982a95e
Rakesh Lawaju <codearryaas@gmail.com>
2017-09-03 22:08:16 +0545

    Remove element check

--- assets/js/wp-travel-front-end-map.js
+++ assets/js/wp-travel-front-end-map.js
@@ -1,25 +1,23 @@
 jQuery( document ).ready( function($){
-	if ( $('#gmap').length > 1 ) {
-		// Create map.
-		var map = new GMaps({
-			div: '#gmap',
-			lat: wp_travel.lat,
-			lng: wp_travel.lng,
-			scrollwheel: false,
-			navigationControl: false,
-			mapTypeControl: false,
-			scaleControl: false,
-			// draggable: false,
-		});
+	// Create map.
+	var map = new GMaps({
+		div: '#gmap',
+		lat: wp_travel.lat,
+		lng: wp_travel.lng,
+		scrollwheel: false,
+		navigationControl: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		// draggable: false,
+	});
 
-		map.setCenter( wp_travel.lat, wp_travel.lng );
-		map.setZoom( 15 );
-		map.addMarker({
-			lat: wp_travel.lat,
-			lng: wp_travel.lng,
-			title: wp_travel.loc,
-			draggable: false
+	map.setCenter( wp_travel.lat, wp_travel.lng );
+	map.setZoom( 15 );
+	map.addMarker({
+		lat: wp_travel.lat,
+		lng: wp_travel.lng,
+		title: wp_travel.loc,
+		draggable: false
 
-		});
-	}
+	});
 } );
