c7bcbf159d1fe47024c4a47f7b1d920b17ef6a96
Racase Lawaju <codearryaas@gmail.com>
2017-10-24 12:47:46 +0545

    shortcode added for listing itineraries

--- inc/class-shortcode.php
+++ inc/class-shortcode.php
@@ -17,27 +17,78 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class Wp_Travel_Shortcodes {
 
+	public function init() {
+	    add_shortcode( 'WP_TRAVEL_ITINERARIES', array( $this, 'wp_travel_get_itineraries_shortcode' ) );
+	    add_shortcode( 'wp_travel_itineraries', array( $this, 'wp_travel_get_itineraries_shortcode' ) );
+ 	}
+
 	/**
 	 * Booking Form.
 	 *
 	 * @return HTMl Html content.
 	 */
-	public static function wp_travel_booking_form() {
-		global $post;
-		ob_start();
-		?>
-		<style>
-			.booking-form form{display: none;}
-		</style>
-		<div class="booking-form">
-			<div class="wp-travel-book-now"><button type="submit" value="submit" class="wp-book-btn default-travel-btn">Book Now</button></div>
-			<?php echo wp_travel_get_booking_form(); ?>
-			<input type="reset" value="Close" class="wp-travel-booking-reset" >
+	public static function wp_travel_get_itineraries_shortcode(  $atts, $content = '' ) {
+
+		$type = isset( $atts['type'] ) ? $atts['type'] : '';
+
+		$id   = isset( $atts['id'] ) ? $atts['id'] : 0;
+		$id   = absint( $id );
+		$slug = isset( $atts['slug'] ) ? $atts['slug'] : '';
+		$limit = isset( $atts['limit'] ) ? $atts['limit'] : 20;
+		$limit = absint( $limit );
+
+
+		$args = array(
+			'post_type' 		=> 'itineraries',
+			'posts_per_page' 	=> $limit,
+			'status'       => 'published',
+		);
+
+		$taxonomies = array( 'itinerary_types', 'travel_locations' );
+		// if type is taxonomy.
+		if ( in_array( $type, $taxonomies ) ) {
+
+			if (  $id > 0 ) {
+				$args['tax_query']	 = array(
+										array(
+											'taxonomy' => $type,
+											'field'    => 'term_id',
+											'terms'    => $id,
+											),
+										);
+			} elseif ( '' !== $slug ) {
+				$args['tax_query']	 = array(
+										array(
+											'taxonomy' => $type,
+											'field'    => 'slug',
+											'terms'    => $slug,
+											),
+										);
+			}
+		} elseif ( 'featured' === $type ) {
+			$args['meta_key']   = 'wp_travel_featured';
+			$args['meta_query'] = array(
+								array(
+									'key'     => 'wp_travel_featured',
+									'value'   => 'yes',
+									// 'compare' => 'IN',
+								),
+							);
+		}
+		$query = new WP_Query( $args ); ?>
+		<div class="wp-travel-itinerary-items">
+			<?php $col_per_row = apply_filters( 'wp_travel_itineraries_col_per_row' , '3' ); ?>
+			<?php if ( $query->have_posts() ) : ?>
+				<ul class="wp-travel-itinerary-list col-<?php esc_attr_e( $col_per_row, 'wp-travel' ) ?>-per-row">
+				<?php while( $query->have_posts() ) : $query->the_post(); ?>
+					<?php wp_travel_get_template_part( 'shortcode/itinerary', 'item' ); ?>
+				<?php endwhile; ?>
+				</ul>
+			<?php else : ?>
+				<?php wp_travel_get_template_part( 'shortcode/itinerary', 'item-none' ); ?>
+			<?php endif; ?>
 		</div>
-		<?php
-		$content = ob_get_contents();
-		ob_end_clean();
-		return apply_filters( 'wp_travel_booking_form_contents', $content );
+		<?php wp_reset_query();
 	}
 
 	/** Send Email after clicking Book Now. */
