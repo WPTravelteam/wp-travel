6e12d3d988ac4f9dd2f4c01c1a3d70c12b4ed162
Racase Lawaju <codearryaas@gmail.com>
2017-11-01 13:40:02 +0545

    widget class included

--- wp-travel.php
+++ wp-travel.php
@@ -148,6 +148,7 @@ if ( ! class_exists( 'WP_Travel' ) ) :
 			include sprintf( '%s/inc/class-taxonomies.php', WP_TRAVEL_ABSPATH );
 			include sprintf( '%s/inc/class-itinerary-template.php', WP_TRAVEL_ABSPATH );
 			include sprintf( '%s/inc/class-shortcode.php', WP_TRAVEL_ABSPATH );
+			include sprintf( '%s/inc/widgets/class-wp-travel-widget-search.php', WP_TRAVEL_ABSPATH );
 
 			if ( $this->is_request( 'admin' ) ) {
 				include sprintf( '%s/inc/admin/admin-helper.php', WP_TRAVEL_ABSPATH );
