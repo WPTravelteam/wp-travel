f5e7339de65b04adbf57e9ea2f4ee9bd896d60ea
Racase Lawaju <codearryaas@gmail.com>
2017-11-19 17:31:57 +0545

    escaping and translation added

--- inc/admin/class-admin-uploader.php
+++ inc/admin/class-admin-uploader.php
@@ -11,6 +11,11 @@
  */
 class WP_Travel_Admin_Uploader {
 
+	/**
+	 * Load Medai upload Form.
+	 *
+	 * @return void
+	 */
 	public function load() {
 		add_action( 'post-plupload-upload-ui', array( $this, 'append_media_upload_form' ) , 1 );
 		add_action( 'post-html-upload-ui', array( $this, 'append_media_upload_form' ) , 1 );
@@ -24,12 +29,17 @@ class WP_Travel_Admin_Uploader {
 		<?php
 	}
 
+	/**
+	 * Media uploader.
+	 *
+	 * @return void
+	 */
 	public function append_media_upload_form() {
 
 			?>
 			<!-- Add from Media Library -->
 			<a href="#" class="wp-travel-open-uploader button" title="<?php esc_html_e( 'Click Here to Insert from Media Library', 'wp-travel' ); ?>" style="vertical-align: baseline;">
-					<?php _e( 'Select Files from Media Library', 'wp-travel' ); ?>
+					<?php esc_html_e( 'Select Files from Media Library', 'wp-travel' ); ?>
 			</a>
 
 			<!-- Progress Bar -->
@@ -37,13 +47,13 @@ class WP_Travel_Admin_Uploader {
 					<div class="wp-travel-upload-progress-bar-inner"></div>
 					<div class="wp-travel-upload-progress-bar-status">
 							<span class="uploading">
-									<?php _e( 'Uploading Image', 'wp-travel' ); ?>
+									<?php esc_html_e( 'Uploading Image', 'wp-travel' ); ?>
 									<span class="current">1</span>
-									<?php _e( 'of', 'wp-travel' ); ?>
+									<?php esc_html_e( 'of', 'wp-travel' ); ?>
 									<span class="total">3</span>
 							</span>
 
-							<span class="done"><?php _e( 'All images uploaded.', 'wp-travel' ); ?></span>
+							<span class="done"><?php esc_html_e( 'All images uploaded.', 'wp-travel' ); ?></span>
 					</div>
 			</div>
 			<?php
