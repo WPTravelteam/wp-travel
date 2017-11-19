bc07cb9e7b0c1ec618697e3a77acb9be2ae98116
Racase Lawaju <codearryaas@gmail.com>
2017-11-19 17:32:14 +0545

    escaping and translation added

--- inc/framework/form/class.form.php
+++ inc/framework/form/class.form.php
@@ -1,9 +1,38 @@
 <?php
+/**
+ * Form Class.
+ *
+ * @package inc/framework/form/
+ */
+
 include WP_TRAVEL_ABSPATH . 'inc/framework/form/class.field.php';
 
+/**
+ * WP Travel Form.
+ * @since 1.0.0
+ */
 class WP_Travel_FW_Form {
+	/**
+	 * Form Option.
+	 *
+	 * @var array
+	 * @since 1.0.0
+	 */
 	private $form_options;
+	/**
+	 * Form Fields
+	 *
+	 * @var array
+	 * @since 1.0.0
+	 */
 	private $fields;
+	/**
+	 * Init Function.
+	 *
+	 * @param array $form_options Attributes of form.
+	 * @since 1.0.0
+	 * @return Obj
+	 */
 	function init( $form_options = array() ) {
 
 		$this->form_options['id'] = isset( $form_options['id'] ) ? $form_options['id'] : '' ;
@@ -21,6 +50,13 @@ class WP_Travel_FW_Form {
 		return $this;
 	}
 
+	/**
+	 * Array list of form fields.
+	 *
+	 * @param array $fields form fields.
+	 * @since 1.0.0
+	 * @return obj
+	 */
 	function fields( $fields ) {
 		$this->fields = $fields;
 		$priority = array();
@@ -31,6 +67,11 @@ class WP_Travel_FW_Form {
 		return $this;
 	}
 
+	/**
+	 * Template
+	 *
+	 * @return void
+	 */
 	function template() {
 		wp_enqueue_script( 'jquery-parsley', plugin_dir_url( WP_TRAVEL_PLUGIN_FILE ) . 'assets/js/lib/parsley/parsley.min.js', array( 'jquery' ) );
 		?>
@@ -45,7 +86,7 @@ class WP_Travel_FW_Form {
 						<?php do_action( $this->form_options['hook_prefix'] . '_before_submit_button' ); ?>
 						<?php wp_nonce_field( $this->form_options['nonce']['action'], $this->form_options['nonce']['field'] ); ?>
 						<?php
-						printf( '<input type="submit" name="%s" id="%s" value="%s">', $this->form_options['submit_button']['name'], $this->form_options['submit_button']['id'], $this->form_options['submit_button']['value'] );
+						printf( '<input type="submit" name="%s" id="%s" value="%s">', esc_attr( $this->form_options['submit_button']['name'], 'wp-travel' ), esc_attr( $this->form_options['submit_button']['id'] ), esc_attr( $this->form_options['submit_button']['value'] ) );
 						?>
 						<?php do_action( $this->form_options['hook_prefix'] . '_after_submit_button' ); ?>
 					</div>
@@ -61,24 +102,32 @@ class WP_Travel_FW_Form {
 		<?php
 	}
 
+	/**
+	 * Slugify.
+	 *
+	 * @param string $string String.
+	 * @param array  $replace Replace String.
+	 * @param string $delimiter Delimiter.
+	 * @return String.
+	 */
 	private function slugify( $string, $replace = array(), $delimiter = '-' ) {
-	  // https://github.com/phalcon/incubator/blob/master/Library/Phalcon/Utils/Slug.php
-	  if (!extension_loaded('iconv')) {
-	    throw new Exception('iconv module not loaded');
-	  }
-	  // Save the old locale and set the new locale to UTF-8
-	  $oldLocale = setlocale(LC_ALL, '0');
-	  setlocale(LC_ALL, 'en_US.UTF-8');
-	  $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
-	  if (!empty($replace)) {
-	    $clean = str_replace((array) $replace, ' ', $clean);
-	  }
-	  $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
-	  $clean = strtolower($clean);
-	  $clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
-	  $clean = trim($clean, $delimiter);
-	  // Revert back to the old locale
-	  setlocale(LC_ALL, $oldLocale);
-	  return $clean;
+		// https://github.com/phalcon/incubator/blob/master/Library/Phalcon/Utils/Slug.php.
+		if ( ! extension_loaded( 'iconv' ) ) {
+			throw new Exception( 'iconv module not loaded' );
+		}
+		// Save the old locale and set the new locale to UTF-8.
+		$old_locale = setlocale( LC_ALL, '0' );
+		setlocale( LC_ALL, 'en_US.UTF-8' );
+		$clean = iconv( 'UTF-8', 'ASCII//TRANSLIT', $string );
+		if ( ! empty( $replace ) ) {
+			$clean = str_replace( ( array ) $replace, ' ', $clean );
+		}
+		$clean = preg_replace( "/[^a-zA-Z0-9\/_|+ -]/", '', $clean );
+		$clean = strtolower( $clean );
+		$clean = preg_replace( "/[\/_|+ -]+/", $delimiter, $clean );
+		$clean = trim( $clean, $delimiter );
+		// Revert back to the old locale.
+		setlocale( LC_ALL, $old_locale );
+		return $clean;
 	}
 }
