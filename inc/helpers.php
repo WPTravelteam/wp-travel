54d73e8d0ff1ae7a443a9bc3fb0632c11531fc51
Racase Lawaju <codearryaas@gmail.com>
2017-12-04 10:13:41 +0545

    function added to get price per and actual trip price

--- inc/helpers.php
+++ inc/helpers.php
@@ -253,6 +253,32 @@ function wp_travel_get_trip_sale_price( $post_id = 0 ) {
 }
 
 /**
+ * Return Trip Price.
+ *
+ * @param  int $post_id Post id of the post.
+ * 
+ * @since 1.0.5
+ * @return int Trip Price.
+ */
+function wp_travel_get_actual_trip_price( $post_id = 0 ) {
+	if ( ! $post_id ) {
+		return 0;
+	}
+
+	$trip_price = get_post_meta( $post_id, 'wp_travel_trip_price', true );
+	if ( ! $trip_price ) {
+		$enable_sale 	= get_post_meta( $post_id, 'wp_travel_enable_sale', true );
+
+		if ( $enable_sale ) {
+			$trip_price = wp_travel_get_trip_sale_price( $post_id );
+		} else {
+			$trip_price = wp_travel_get_trip_price( $post_id );
+		}
+	}
+	return $trip_price;	
+}
+
+/**
  * Get post thumbnail.
  *
  * @param  int    $post_id Post ID.
@@ -546,4 +572,20 @@ function wp_travel_get_trip_duration( $post_id ) {
 			</span>
 		</div>
 	<?php endif; 
+}
+
+/**
+ * Get Price Per text.
+ * 
+ * @since 1.0.5
+ */
+function wp_travel_get_price_per_text( $post_id ) {
+	if ( ! $post_id ) {
+		return;
+	}
+	$per_person_text = get_post_meta( $post_id, 'wp_travel_price_per', true );
+	if ( ! $per_person_text ) {
+		$per_person_text = 'person';
+	}
+	return $per_person_text;
 }
