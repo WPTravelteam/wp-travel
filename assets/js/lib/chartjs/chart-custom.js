e801873793acbd5501be8578fcb5dd38643625fb
Racase Lawaju <codearryaas@gmail.com>
2017-12-15 14:23:23 +0545

    added view more country

--- assets/js/lib/chartjs/chart-custom.js
+++ assets/js/lib/chartjs/chart-custom.js
@@ -104,4 +104,38 @@ jQuery(document).ready(function($) {
         }
 
     });
+
+    // Show more link on top country
+    var showChar = wp_travel_chart_data.show_char;
+    var ellipsestext = "..";
+    var moretext = wp_travel_chart_data.show_more_text;
+    var lesstext = wp_travel_chart_data.show_less_text;
+    $('.wp-travel-more').each(function() {
+        var content = $(this).html();
+
+        if (content.length > showChar) {
+
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+
+            var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+            $(this).html(html);
+        }
+
+    });
+
+    $(".morelink").click(function() {
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+
 })
