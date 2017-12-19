09aa505807e1663c65ab15fa77596d39d5cffc3f
Racase Lawaju <codearryaas@gmail.com>
2017-12-19 14:09:46 +0545

    text updated

--- inc/admin/class-admin-menu.php
+++ inc/admin/class-admin-menu.php
@@ -5,7 +5,7 @@ class WP_Travel_Admin_Menu {
 		add_action( 'admin_menu', array( $this, 'add_menus' ) );
 	}
 	function add_menus() {
-		add_submenu_page( 'edit.php?post_type=itineraries', __( 'Statististics', 'wp-travel' ), __( 'Statististics', 'wp-travel' ), 'manage_options', 'booking_chart', 'get_booking_chart' );
+		add_submenu_page( 'edit.php?post_type=itineraries', __( 'Stat', 'wp-travel' ), __( 'Stat', 'wp-travel' ), 'manage_options', 'booking_chart', 'get_booking_chart' );
 		add_submenu_page( 'edit.php?post_type=itineraries', __( 'WP Travel Settings', 'wp-travel' ), __( 'Settings', 'wp-travel' ), 'manage_options', 'settings', array( 'WP_Travel_Admin_Settings', 'setting_page_callback' ) );
 
 		add_submenu_page( 'edit.php?post_type=itineraries', __( 'System Status', 'wp-travel' ), __( 'Status', 'wp-travel' ), 'manage_options', 'sysinfo', array( 'WP_Travel_Admin_Settings', 'get_system_info' ) );
