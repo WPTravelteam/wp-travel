84ec3e81c95e3708aa9a118905d56be450bfd1ff
Racase Lawaju <codearryaas@gmail.com>
2018-01-09 18:08:50 +0545

    standard updated

--- inc/payments/wp-travel-payments.php
+++ inc/payments/wp-travel-payments.php
@@ -1,29 +1,29 @@
 <?php
-
 interface Wp_Travel_Payment_Interface {
-    public function process_payment();
+	public function process_payment();
+	
+	public function render_settings();
 }
 
 $GLOBALS['wp_travel_payments'] = [];
 
-
 if ( ! function_exists( 'wp_travel_register_payments' ) ) {
 
-    /**
-     * Register payments here
-     */
-    function wp_travel_register_payments($object){
+	/**
+	 * Register payments here
+	 *
+	 * @param Object $object Payment Object.
+	 */
+	function wp_travel_register_payments( $object ) {
 
-        if(!is_object($object)){
-            throw new \Exception('Payment gateway must be an instance of class.'.gettype($object).' given.');
-        }
+		if ( ! is_object( $object ) ) {
+			throw new \Exception( 'Payment gateway must be an instance of class. ' . gettype( $object ) . ' given.' );
+		}
 
-        if(!($object instanceof Wp_Travel_Payment_Interface)){
-            throw new \Exception('Payment gateway must be an instance of Wp_Travel_Payment_Interface. Instance of '.get_class($object).' given.');
-        }
+		if ( ! ( $object instanceof Wp_Travel_Payment_Interface ) ) {
+			throw new \Exception( 'Payment gateway must be an instance of Wp_Travel_Payment_Interface. Instance of ' . get_class( $object ) . ' given.' );
+		}
 
-        array_push($GLOBALS['wp_travel_payments'],$object);
-
-    }
-
+		array_push( $GLOBALS['wp_travel_payments'], $object );
+	}
 }
