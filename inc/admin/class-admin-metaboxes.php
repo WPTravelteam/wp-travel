5fb0e4dfbe403d0e00c424fbe8cd66043fb1bb37
Racase Lawaju <codearryaas@gmail.com>
2018-01-12 10:01:08 +0545

    admin edit layout fixes

--- inc/admin/class-admin-metaboxes.php
+++ inc/admin/class-admin-metaboxes.php
@@ -21,7 +21,7 @@ class WP_Travel_Admin_Metaboxes {
 	public function __construct() {
 		add_action( 'add_meta_boxes', array( $this, 'register_metaboxes' ), 10, 2 );
 		add_action( 'do_meta_boxes', array( $this, 'remove_metaboxs' ), 10, 2 );
-		add_filter( 'postbox_classes_itineraries_wp-travel-itinerary-detail', array( $this, 'add_clean_metabox_class' ) );
+		add_filter( 'postbox_classes_' . WP_TRAVEL_POST_TYPE . '_wp-travel-' . WP_TRAVEL_POST_TYPE . '-detail', array( $this, 'add_clean_metabox_class' ) );
 		add_filter( 'wp_travel_admin_tabs', array( $this, 'add_tabs' ) );
 		add_action( 'admin_footer', array( $this, 'gallery_images_listing' ) );
 		add_action( 'save_post', array( $this, 'save_meta_data' ) );
