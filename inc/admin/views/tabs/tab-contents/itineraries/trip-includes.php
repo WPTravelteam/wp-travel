0785ecc21743e8140d2eb577cc1f44c81d1c89c2
Racase Lawaju <codearryaas@gmail.com>
2018-01-12 14:21:21 +0545

    content moved

--- inc/admin/views/tabs/tab-contents/itineraries/trip-includes.php
+++ inc/admin/views/tabs/tab-contents/itineraries/trip-includes.php
@@ -1,10 +1,13 @@
 <?php
 	global $post;
 	$trip_include = get_post_meta( $post->ID, 'wp_travel_trip_include', true );
+	$trip_exclude = get_post_meta( $post->ID, 'wp_travel_trip_exclude', true );
 ?>
 <table class="form-table">	
 	<tr>
-		<td><label for="wp_travel_trip_include"><?php esc_html_e( 'Trip Includes', 'wp-travel' ); ?></label></td>
-		<td><?php wp_editor( $trip_include, 'wp_travel_trip_include' ); ?></td>
+		<td><h4><label for="wp_travel_trip_include"><?php esc_html_e( 'Trip Includes', 'wp-travel' ); ?></label></h4><?php wp_editor( $trip_include, 'wp_travel_trip_include' ); ?></td>
 	</tr>
+	<tr>
+		<td><h4><label for="wp_travel_trip_exclude"><?php esc_html_e( 'Trip Excludes', 'wp-travel' ); ?></label></h4><?php wp_editor( $trip_exclude, 'wp_travel_trip_exclude' ); ?></td>
+	</tr>	
 </table>
