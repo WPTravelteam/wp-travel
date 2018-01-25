20e8c0e8bb32918268d75c42bf4ec227e44a49b6
Sus Hill <dhakal.sushil@wensolutions.com>
2018-01-25 18:03:35 +0545

    global tab setting styling

--- assets/js/wp-travel-tabs.js
+++ assets/js/wp-travel-tabs.js
@@ -20,7 +20,7 @@
     }
 
     // Sortable for Global tabs.
-    $('.wp-travel-sorting-tabs').sortable({
+    $('#wp-travel-tab-content-tabs_global .wp-travel-sorting-tabs tbody').sortable({
         handle: '.wp-travel-sorting-handle'
     });
 
--- inc/admin/class-admin-settings.php
+++ inc/admin/class-admin-settings.php
@@ -215,23 +215,40 @@ class WP_Travel_Admin_Settings {
 		}
 		
 		if ( is_array( $global_tabs ) && count( $global_tabs ) > 0 ) {
-			echo '<ul class="wp-travel-sorting-tabs">';
+			echo '<table class="wp-travel-sorting-tabs form-table">';
+		?>
+				<thead>
+					<th width="50px">Sorting</th>
+					<th width="70%">Tittle</th>
+					<th width="20%"><?php esc_html_e( 'Display', 'wp-travel' ); ?></th>
+				</thead>
+				<tbody>
+			<?php 
 			foreach ( $global_tabs as $key => $tab ) : ?>
-				<li class="clearfix">
-					<div class="wp-travel-sorting-handle"></div>
-					<div class="wp-travel-sorting-tabs-wrap">
+				<tr>
+					<td width="50px">
+						<div class="wp-travel-sorting-handle">
+						</div>
+					</td>
+					<td width="70%">
+						<div class="wp-travel-sorting-tabs-wrap">
 						<span class="wp-travel-tab-label wp-travel-accordion-title"><?php echo esc_html( $tab['label'] ); ?></span>
 						<input type="text" class="wp_travel_tabs_input-field section_title" name="wp_travel_global_tabs_settings[<?php echo esc_attr( $key ) ?>][label]" value="<?php echo esc_html( $tab['label'] ); ?>" placeholder="<?php echo esc_html( $tab['label'] ); ?>" />
 						<input type="hidden" name="wp_travel_global_tabs_settings[<?php echo esc_attr( $key ) ?>][show_in_menu]" value="no" />
+						
+					</div>
+					</td>
+					<td width="20%">
 						<span class="show-in-frontend checkbox-default-design"><label data-on="ON" data-off="OFF"><input name="wp_travel_global_tabs_settings[<?php echo esc_attr( $key ) ?>][show_in_menu]" type="checkbox" value="yes" <?php checked( 'yes', $tab['show_in_menu'] ) ?> /><?php //esc_html_e( 'Display', 'wp-travel' ); ?>
 						<span class="switch">
 						  </span>
 						</label></span>
-					</div>
-				</li>
+					</td>
+					
+				</tr>
 			<?php
 				endforeach;
-			echo'</ul>';
+			echo'<tbody></table>';
 		}
 	}
 
