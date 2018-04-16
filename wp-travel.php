ce5bede12f71fdaeb3b439e9651de91bd8a85293
Rakesh Lawaju <rakesh.lawaju@incsub.com>
2018-04-16 17:20:33 +0545

    Changes

--- wp-travel.php
+++ wp-travel.php
@@ -35,7 +35,7 @@ if ( ! class_exists( 'WP_Travel' ) ) :
 		 *
 		 * @var string
 		 */
-		public $version = '1.3.0';
+		public $version = '1.3.1';
 		/**
 		 * The single instance of the class.
 		 *
