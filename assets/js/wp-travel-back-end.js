617eb9573eeb698425b005cd30c616bafb647f86
avee_shake <rijal.abhishek@wensolutions.com>
2018-04-25 15:26:21 +0545

    sale price issue fixing

--- assets/js/wp-travel-back-end.js
+++ assets/js/wp-travel-back-end.js
@@ -195,9 +195,11 @@
         if ($(this).is(':checked')) {
             $('#wp-travel-sale-price').removeAttr('disabled').closest('tr').show();
             $('#wp-travel-price').attr('required', 'required');
+            $('#wp-travel-sale-price').attr('required', 'required');
         } else {
             $('#wp-travel-sale-price').attr('disabled', 'disabled').closest('tr').hide();
             $('#wp-travel-price').removeAttr('required');
+            $('#wp-travel-sale-price').removeAttr('required');
         }
     });
 
@@ -274,6 +276,7 @@
 
     if ($('#wp-travel-enable-sale').is(':checked')) {
         $('#wp-travel-price').attr('required', 'required');
+        $('#wp-travel-sale-price').attr('required', 'required');
     }
 
     $(document).on('click', '#wp-travel-fixed-departure', function() {
