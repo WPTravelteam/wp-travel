fe66ceff8d80a2463959093130b0c5b52e72efc2
avee_shake <rijal.abhishek@wensolutions.com>
2018-05-18 15:51:00 +0545

    email additionals

--- inc/class-post-types.php
+++ inc/class-post-types.php
@@ -54,7 +54,7 @@ class WP_Travel_Post_Types {
 			'hierarchical'       => false,
 			'menu_position'      => null,
 			'supports'           => array( 'title', 'comments' ),
-			'menu_icon'          => 'dashicons-wp-travelcle',
+			'menu_icon'          => 'dashicons-wp-travel',
 		);
 		/**
 		 * Register a itineraries post type.
