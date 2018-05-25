4ccddbab2efdf9fcd37b1d23100bfd077c09a1dc
sus hill <dhakal.sushil@wensolutions.com>
2018-05-25 11:29:03 +0545

    reset password and new lost password page design

--- templates/account/form-lostpassword.php
+++ templates/account/form-lostpassword.php
@@ -14,24 +14,42 @@ if ( ! defined( 'ABSPATH' ) ) {
 WP_Travel()->notices->print_notices( 'error', true );
 ?>
 
-<form method="post" class="wp-travel-ResetPassword lost_reset_password">
 
-	<p><?php echo apply_filters( 'wp_travel_lost_password_message', esc_html__( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'wp-travel' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
 
-	<p class="wp-travel-form-row wp-travel-form-row--first form-row form-row-first">
-		<label for="user_login"><?php esc_html_e( 'Username or email', 'wp-travel' ); ?></label>
-		<input class="wp-travel-Input wp-travel-Input--text input-text" type="text" name="user_login" id="user_login" />
-	</p>
+<div class="wp-travel-dashboard-form">
+	<div class="login-page">
+		<?php if ( has_custom_logo() ) : ?>
+			<div class="login-logo">
+				<?php the_custom_logo(); ?>
+			</div>
+		<?php endif; ?>
+		<div class="form">
+			<form method="post" class="wp-travel-ResetPassword lost_reset_password">
+				<h3>Reset Password</h3>
+				<p><?php echo apply_filters( 'wp_travel_lost_password_message', esc_html__( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'wp-travel' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
 
-	<div class="clear"></div>
+				<p class="wp-travel-form-row wp-travel-form-row--first form-row form-row-first">
+				<span class="user-username">
+					<!-- <label for="user_login"><?php esc_html_e( 'Username or email', 'wp-travel' ); ?></label> -->
+					<input class="wp-travel-Input wp-travel-Input--text input-text" type="text" name="user_login" id="user_login" placeholder="email or username" />
+				</span>	
+				</p>
 
-	<?php do_action( 'wp_travel_lostpassword_form' ); ?>
+				<div class="clear"></div>
 
-	<p class="wp-travel-form-row form-row">
-		<input type="hidden" name="wp_travel_reset_password" value="true" />
-		<button type="submit" class="wp-travel-Button button" value="<?php esc_attr_e( 'Reset password', 'wp-travel' ); ?>"><?php esc_html_e( 'Reset password', 'wp-travel' ); ?></button>
-	</p>
+				<?php do_action( 'wp_travel_lostpassword_form' ); ?>
 
-	<?php wp_nonce_field( 'wp_travel_lost_password' ); ?>
+				<p class="wp-travel-form-row form-row">
+					<input type="hidden" name="wp_travel_reset_password" value="true" />
+					<button type="submit" class="wp-travel-Button button" value="<?php esc_attr_e( 'Reset password', 'wp-travel' ); ?>"><?php esc_html_e( 'Reset password', 'wp-travel' ); ?></button>
+				</p>
 
-</form>
+				<?php wp_nonce_field( 'wp_travel_lost_password' ); ?>
+
+			</form>
+
+		</div>
+	</div>
+</div>
+
+
--- templates/account/form-reset-password.php
+++ templates/account/form-reset-password.php
@@ -22,31 +22,50 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Print Errors / Notices.
 WP_Travel()->notices->print_notices( 'error', true ); ?>
 
-<form method="post" class="wp-travel-ResetPassword lost_reset_password">
 
-	<p><?php echo apply_filters( 'wp_travel_reset_password_message', esc_html__( 'Enter a new password below.', 'wp-travel' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
 
-	<p class="wp-travel-form-row wp-travel-form-row--first form-row form-row-first">
-		<label for="password_1"><?php esc_html_e( 'New password', 'wp-travel' ); ?> <span class="required">*</span></label>
-		<input type="password" class="wp-travel-Input wp-travel-Input--text input-text" name="password_1" id="password_1" />
-	</p>
-	<p class="wp-travel-form-row wp-travel-form-row--last form-row form-row-last">
-		<label for="password_2"><?php esc_html_e( 'Re-enter new password', 'wp-travel' ); ?> <span class="required">*</span></label>
-		<input type="password" class="wp-travel-Input wp-travel-Input--text input-text" name="password_2" id="password_2" />
-	</p>
+<div class="wp-travel-dashboard-form">
+	<div class="login-page">
+		<?php if ( has_custom_logo() ) : ?>
+			<div class="login-logo">
+				<?php the_custom_logo(); ?>
+			</div>
+		<?php endif; ?>
+		<div class="form">
+			<form method="post" class="wp-travel-ResetPassword lost_reset_password">
+				<h3><?php echo apply_filters( 'wp_travel_reset_password_message', esc_html__( 'Enter a new password below.', 'wp-travel' ) ); ?></h3>
+				<p></p><?php // @codingStandardsIgnoreLine ?>
 
-	<input type="hidden" name="reset_key" value="<?php echo esc_attr( $args['key'] ); ?>" />
-	<input type="hidden" name="reset_login" value="<?php echo esc_attr( $args['login'] ); ?>" />
+				<p class="wp-travel-form-row wp-travel-form-row--first form-row form-row-first">
+					<!-- <label for="password_1"><?php esc_html_e( 'New password', 'wp-travel' ); ?> <span class="required">*</span></label> -->
+					<span class="user-password">
+						<input type="password" placeholder="<?php esc_html_e( 'New password', 'wp-travel' ); ?>" class="wp-travel-Input wp-travel-Input--text input-text" name="password_1" id="password_1" />
+					</span>
+				</p>
+				<p class="wp-travel-form-row wp-travel-form-row--last form-row form-row-last">
+					<!-- <label for="password_2"><?php esc_html_e( 'Re-enter new password', 'wp-travel' ); ?> <span class="required">*</span></label> -->
+					<span class="user-password-key">
+						<input type="password" placeholder="<?php esc_html_e( 'Re-enter new password', 'wp-travel' ); ?>" class="wp-travel-Input wp-travel-Input--text input-text" name="password_2" id="password_2" />
+					</span>	
+				</p>
 
-	<div class="clear"></div>
+				<input type="hidden" name="reset_key" value="<?php echo esc_attr( $args['key'] ); ?>" />
+				<input type="hidden" name="reset_login" value="<?php echo esc_attr( $args['login'] ); ?>" />
 
-	<?php do_action( 'wp_travel_resetpassword_form' ); ?>
+				<div class="clear"></div>
 
-	<p class="wp-travel-form-row form-row">
-		<input type="hidden" name="wp_travel_reset_password" value="true" />
-		<button type="submit" class="wp-travel-Button button" value="<?php esc_attr_e( 'Save', 'wp-travel' ); ?>"><?php esc_html_e( 'Save', 'wp-travel' ); ?></button>
-	</p>
+				<?php do_action( 'wp_travel_resetpassword_form' ); ?>
 
-	<?php wp_nonce_field( 'wp_travel_reset_password_nonce' ); ?>
+				<p class="wp-travel-form-row form-row">
+					<input type="hidden" name="wp_travel_reset_password" value="true" />
+					<button type="submit" class="wp-travel-Button button" value="<?php esc_attr_e( 'Save', 'wp-travel' ); ?>"><?php esc_html_e( 'Save', 'wp-travel' ); ?></button>
+				</p>
 
-</form>
+				<?php wp_nonce_field( 'wp_travel_reset_password_nonce' ); ?>
+
+			</form>
+
+		</div>
+	</div>
+</div>
+
