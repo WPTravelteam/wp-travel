ff9630a9e1befebb8f6f1efab30f89e341792d24
avee_shake <rijal.abhishek@gmail.com>
2018-10-10 09:35:54 +0545

    norwegein

---
+++ assets/js/lib/datepicker/i18n/datepicker.no.js
@@ -0,0 +1,15 @@
+;
+(function ($) { 
+    $.fn.datepicker.language['no'] = { 
+        days: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'], 
+        daysShort: ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Løradg'], 
+        daysMin: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø'], 
+        months: ['Januar','Februar','Mars','April','Mai','Juni', 'July','August','September','Oktober','November','Desember'], 
+        monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des'], 
+        today: 'I dag', 
+        clear: 'Clear', 
+        dateFormat: 'dd/mm/yyyy', 
+        timeFormat: 'hh:ii aa', 
+        firstDay: 0 
+    }; 
+})(jQuery);
