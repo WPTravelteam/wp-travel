2a58abcd73f5d32fa6b6d9b609c58b69db0f96f8
Rakesh <codearryaas@gmail.com>
2018-11-02 16:55:24 +0545

    email charset updated

--- inc/class-email-template.php
+++ inc/class-email-template.php
@@ -24,7 +24,7 @@ class WP_Travel_Emails {
 
 		// To send HTML mail, the Content-type header must be set.
 		$headers  = 'MIME-Version: 1.0' . "\r\n";
-		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 		
 		if ( $from ) :
 			// Create email headers.
