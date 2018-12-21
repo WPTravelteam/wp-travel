bdd09206164ef07a44cfcbd81cb80fca19bbb9e8
Rakesh <codearryaas@gmail.com>
2018-12-21 16:55:42 +0545

    admin map hooked into wp_travel_admin_map_area

--- inc/admin/views/tabs/tab-contents/itineraries/location-tab.php
+++ inc/admin/views/tabs/tab-contents/itineraries/location-tab.php
@@ -3,6 +3,7 @@ global $post;
 
 $map_data = get_wp_travel_map_data();
 $settings = wp_travel_get_settings();
+add_action( 'wp_travel_admin_map_area', 'wp_travel_google_map', 10, 2 );
 
 ?>
 <h4><?php _e( 'Destination', 'wp-travel'); ?></h4>
@@ -15,23 +16,14 @@ $settings = wp_travel_get_settings();
 
 <h4><?php _e( 'Map', 'wp-travel'); ?></h4>
 <div class="map-wrap">
-  <?php if ( isset( $settings['google_map_api_key'] ) && '' != $settings['google_map_api_key'] ) : ?>
-  <input id="search-input" class="controls" type="text" placeholder="Enter a location" value="<?php echo esc_html( $map_data['loc'] ); ?>" >
-  <div id="gmap" style="width:100%;height:300px"></div>
-  <input type="hidden" name="wp_travel_location" id="wp-travel-location" value="<?php echo esc_html( $map_data['loc'] ); ?>" >
-  <input type="hidden" name="wp_travel_lat" id="wp-travel-lat" value="<?php echo esc_html( $map_data['lat'] ); ?>" >
-  <input type="hidden" name="wp_travel_lng" id="wp-travel-lng" value="<?php echo esc_html( $map_data['lng'] ); ?>" >
-<?php else : ?>
-  <div class="map-wrap">
-  <p class="good" id="pass-strength-result"><?php echo sprintf( "Please add 'google map api key' in the <a href=\"edit.php?post_type=" . WP_TRAVEL_POST_TYPE . "&page=settings\">settings</a>" ) ?></p>
+  <?php do_action( 'wp_travel_admin_map_area', $settings, $map_data ); ?>
+  <br>
   <div class="wp-travel-upsell-message">
-			<div class="wp-travel-pro-feature-notice">
-				<h4><?php esc_html_e( 'Need alternative maps ?', 'wp-travel' ); ?></h4>
-				<p><?php printf( __( 'If you need alternative to current map then you can get free or pro maps for WP Travel. %1$sView WP Travel Map addons%2$s', 'wp-travel' ), '<br><a target="_blank" href="https://wptravel.io/downloads/category/map/">', '</a>' ); ?></p>
-			</div>
-		</div>
+    <div class="wp-travel-pro-feature-notice">
+      <h4><?php esc_html_e( 'Need alternative maps ?', 'wp-travel' ); ?></h4>
+      <p><?php printf( __( 'If you need alternative to current map then you can get free or pro maps for WP Travel. %1$sView WP Travel Map addons%2$s', 'wp-travel' ), '<br><a target="_blank" href="https://wptravel.io/downloads/category/map/">', '</a>' ); ?></p>
+    </div>
   </div>
-<?php endif; ?>
 </div>
 <style>
 .map-wrap{
@@ -65,3 +57,36 @@ $settings = wp_travel_get_settings();
     border-color: #4d90fe;
 }
 </style>
+
+<?php
+
+function wp_travel_google_map( $settings, $map_data ) {
+
+  $api_key = '';
+
+  $get_maps = wp_travel_get_maps();
+  $current_map = $get_maps['selected'];
+
+  $show_google_map = ( 'google-map' === $current_map ) ? true : false;
+  $show_google_map = apply_filters( 'wp_travel_load_google_maps_api', $show_google_map );
+
+  if ( isset( $settings['google_map_api_key'] ) && '' != $settings['google_map_api_key'] ) {
+    $api_key = $settings['google_map_api_key'];
+  }
+
+  if ( $show_google_map ) {
+    if ( '' != $api_key ) : ?>
+      <input id="search-input" class="controls" type="text" placeholder="Enter a location" value="<?php echo esc_html( $map_data['loc'] ); ?>" >
+      <div id="gmap" style="width:100%;height:300px"></div>
+      <input type="hidden" name="wp_travel_location" id="wp-travel-location" value="<?php echo esc_html( $map_data['loc'] ); ?>" >
+      <input type="hidden" name="wp_travel_lat" id="wp-travel-lat" value="<?php echo esc_html( $map_data['lat'] ); ?>" >
+      <input type="hidden" name="wp_travel_lng" id="wp-travel-lng" value="<?php echo esc_html( $map_data['lng'] ); ?>" >
+    <?php else : ?>
+      <div class="map-wrap">
+      <p class="good" id="pass-strength-result"><?php echo sprintf( "Please add 'google map api key' in the <a href=\"edit.php?post_type=" . WP_TRAVEL_POST_TYPE . "&page=settings\">settings</a>" ) ?></p>
+    </div>
+    <?php endif;
+  }
+}
+
+
