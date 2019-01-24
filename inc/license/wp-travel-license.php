a3320195239af5275a1cb8b2367a45a6c8984c1f
Rakesh <codearryaas@gmail.com>
2019-01-24 17:49:11 +0545

    1.8.0

--- inc/license/wp-travel-license.php
+++ inc/license/wp-travel-license.php
@@ -12,19 +12,13 @@ class WP_Travel_License {
 	 */
 	private static $store_url;
 
+	/**
+	 * Premium Addons List.
+	 */
 	private static $addons = array();
 
 	public function __construct() {
-		self::$store_url = ( defined( 'WP_TRAVEL_TESTING_STORE_URL' ) && '' !== WP_TRAVEL_TESTING_STORE_URL ) ? WP_TRAVEL_TESTING_STORE_URL : self::LIVE_STORE_URL;
 
-		$premium_addons = apply_filters( 'wp_travel_premium_addons_list', array() );
-		if ( count( $premium_addons ) > 0 ) {
-			foreach ( $premium_addons as $key => $premium_addon ) {
-				if ( is_array( $premium_addon ) ) {
-					self::$addons[ $key ] = $premium_addon;
-				}
-			}
-		}
 	}
 
 	public static function count_premium_addons() {
@@ -36,7 +30,16 @@ class WP_Travel_License {
 	 * @return void
 	 */
 	public static function init() {
+		self::$store_url = ( defined( 'WP_TRAVEL_TESTING_STORE_URL' ) && '' !== WP_TRAVEL_TESTING_STORE_URL ) ? WP_TRAVEL_TESTING_STORE_URL : self::LIVE_STORE_URL;
 
+		$premium_addons = apply_filters( 'wp_travel_premium_addons_list', array() );
+		if ( count( $premium_addons ) > 0 ) {
+			foreach ( $premium_addons as $key => $premium_addon ) {
+				if ( is_array( $premium_addon ) ) {
+					self::$addons[ $key ] = $premium_addon;
+				}
+			}
+		}
 
 		add_action( 'admin_init', 'WP_Travel_License::plugin_updater', 0 );
 		add_action( 'wp_travel_license_tab_fields', 'WP_Travel_License::setting_fields' );
@@ -54,15 +57,18 @@ class WP_Travel_License {
 	 */
 	public static function plugin_updater() {
 		$settings             = get_option( 'wp_travel_settings' );
-		$count_premium_addons = WP_Travel_License::count_premium_addons();
+		$count_premium_addons = self::count_premium_addons();
 		if ( $count_premium_addons > 0 ) {
 			foreach ( self::$addons as $key => $premium_addon ) {
 				// retrieve the license from the database.
 				$license_key = ( isset( $settings[ $premium_addon['_option_prefix'] . 'key' ] ) ) ? $settings[ $premium_addon['_option_prefix'] . 'key' ] : '';
-				$args        = wp_parse_args( $premium_addon, array(
-					'license' => $license_key,
-					'author'  => 'WEN Solutions',
-				) );
+				$args        = wp_parse_args(
+					$premium_addon,
+					array(
+						'license' => $license_key,
+						'author'  => 'WEN Solutions',
+					)
+				);
 				unset( $args['_option_prefix'] );
 				unset( $args['_file_path'] );
 
@@ -80,15 +86,15 @@ class WP_Travel_License {
 	 * @return void
 	 */
 	public static function setting_fields( $settings_args ) {
-		$count_premium_addons = WP_Travel_License::count_premium_addons();
+		$count_premium_addons = self::count_premium_addons();
 		if ( $count_premium_addons > 0 ) {
-		$premium_addons = self::$addons;
-		$settings       = isset( $settings_args['settings'] ) ? $settings_args['settings'] : array();
-		foreach ( $premium_addons as $key => $premium_addon ) :
-			// Get license status.
-			$status      = get_option( $premium_addon['_option_prefix'] . 'status' );
-			$license_key = isset( $settings[ $premium_addon['_option_prefix'] . 'key' ] ) ? $settings[ $premium_addon['_option_prefix'] . 'key' ] : '';
-		?>
+			$premium_addons = self::$addons;
+			$settings       = isset( $settings_args['settings'] ) ? $settings_args['settings'] : array();
+			foreach ( $premium_addons as $key => $premium_addon ) :
+				// Get license status.
+				$status      = get_option( $premium_addon['_option_prefix'] . 'status' );
+				$license_key = isset( $settings[ $premium_addon['_option_prefix'] . 'key' ] ) ? $settings[ $premium_addon['_option_prefix'] . 'key' ] : '';
+				?>
 		<style>
 			#wp-travel-tab-content-license .form-table input[class*=button].button-license{height:35px;line-height:35px}
 		</style>
@@ -97,9 +103,9 @@ class WP_Travel_License {
 			<table class="form-table">
 				<tbody>
 					<tr>
-						<th><label for="<?php echo $key; ?>-license-key"><?php esc_html_e( 'License Key', 'wp-travel' ) ?></label></th>
+						<th><label for="<?php echo $key; ?>-license-key"><?php esc_html_e( 'License Key', 'wp-travel' ); ?></label></th>
 						<td>
-							<input type="text" value="<?php echo esc_attr( $license_key ) ?>" name="<?php echo $premium_addon['_option_prefix']; ?>key" id="<?php echo $key; ?>-license-key">
+							<input type="text" value="<?php echo esc_attr( $license_key ); ?>" name="<?php echo $premium_addon['_option_prefix']; ?>key" id="<?php echo $key; ?>-license-key">
 							<?php if ( $license_key ) : ?>
 								<span class="license-status-warp" style="position:relative;" >
 									<span style="position:absolute;top:0;right:10px">
@@ -109,7 +115,7 @@ class WP_Travel_License {
 										<span style="color:red;"><?php esc_html_e( 'Invalid', 'wp-travel' ); ?></span>
 									<?php elseif ( 'expired' === $status ) : ?>
 										<span style="color:red;"><?php esc_html_e( 'Expired', 'wp-travel' ); ?></span>
-									<?php elseif ( 'inactive' === $status ) :  ?>
+									<?php elseif ( 'inactive' === $status ) : ?>
 										<span style="color:orange;"><?php esc_html_e( 'Inactive', 'wp-travel' ); ?></span>
 									<?php endif; ?>
 									</span>
@@ -121,9 +127,9 @@ class WP_Travel_License {
 								<?php wp_nonce_field( $premium_addon['_option_prefix'] . 'nonce', $premium_addon['_option_prefix'] . 'nonce' ); ?>
 
 								<?php if ( false !== $status && 'valid' === $status ) { ?>
-									<input type="submit" class="button button-secondary button-license" name="<?php echo $premium_addon['_option_prefix']; ?>deactivate" value="<?php esc_html_e( 'Deactivate License', 'wp-travel' ) ?>" />
+									<input type="submit" class="button button-secondary button-license" name="<?php echo $premium_addon['_option_prefix']; ?>deactivate" value="<?php esc_html_e( 'Deactivate License', 'wp-travel' ); ?>" />
 								<?php } else { ?>
-									<input type="submit" class="button button-primary button-license" name="<?php echo $premium_addon['_option_prefix']; ?>activate" value="<?php esc_html_e( 'Activate License', 'wp-travel' ) ?>" />
+									<input type="submit" class="button button-primary button-license" name="<?php echo $premium_addon['_option_prefix']; ?>activate" value="<?php esc_html_e( 'Activate License', 'wp-travel' ); ?>" />
 								<?php } ?>
 								<input type="hidden" name="save_settings_button" value="true" />
 
@@ -134,7 +140,7 @@ class WP_Travel_License {
 			</table>
 		</div>
 
-	<?php
+				<?php
 			endforeach;
 		}
 		?>
@@ -158,14 +164,14 @@ class WP_Travel_License {
 		if ( ! $settings ) {
 			return;
 		}
-		$count_premium_addons = WP_Travel_License::count_premium_addons();
+		$count_premium_addons = self::count_premium_addons();
 		if ( $count_premium_addons < 1 ) {
 			return $settings;
 		}
 		$premium_addons = self::$addons;
 		foreach ( $premium_addons as $key => $premium_addon ) {
-			$key_option_name = $premium_addon['_option_prefix'] . 'key';
-			$license_key     = ( isset( $_POST[ $key_option_name ] ) && '' !== $_POST[ $key_option_name ] ) ? $_POST[ $key_option_name ] : '';
+			$key_option_name              = $premium_addon['_option_prefix'] . 'key';
+			$license_key                  = ( isset( $_POST[ $key_option_name ] ) && '' !== $_POST[ $key_option_name ] ) ? $_POST[ $key_option_name ] : '';
 			$settings[ $key_option_name ] = $license_key;
 		}
 		return $settings;
@@ -177,7 +183,7 @@ class WP_Travel_License {
 	 * @return void
 	 */
 	public static function activate_license() {
-		$count_premium_addons = WP_Travel_License::count_premium_addons();
+		$count_premium_addons = self::count_premium_addons();
 		if ( $count_premium_addons < 1 ) {
 			return;
 		}
@@ -187,8 +193,8 @@ class WP_Travel_License {
 			if ( isset( $_POST[ $premium_addon['_option_prefix'] . 'activate' ] ) ) {
 				delete_transient( $premium_addon['_option_prefix'] . 'data' );
 				// run a quick security check.
-			 	if ( ! check_admin_referer( $premium_addon['_option_prefix'] . 'nonce', $premium_addon['_option_prefix'] . 'nonce' ) ) {
-					 return; // get out if we didn't click the Activate button.
+				if ( ! check_admin_referer( $premium_addon['_option_prefix'] . 'nonce', $premium_addon['_option_prefix'] . 'nonce' ) ) {
+					return; // get out if we didn't click the Activate button.
 				}
 
 				// retrieve the license from the database.
@@ -203,11 +209,18 @@ class WP_Travel_License {
 				);
 
 				// Call the custom API.
-				$response = wp_remote_post( self::$store_url, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+				$response = wp_remote_post(
+					self::$store_url,
+					array(
+						'timeout'   => 15,
+						'sslverify' => false,
+						'body'      => $api_params,
+					)
+				);
 
 				// make sure the response came back okay.
 				if ( is_wp_error( $response ) ) {
-					error_log( print_r( $response, true ));
+					error_log( print_r( $response, true ) );
 					return false;
 				}
 
@@ -216,7 +229,6 @@ class WP_Travel_License {
 
 				// Set license data trasient.
 				set_transient( $premium_addon['_option_prefix'] . 'data', $license_data, 12 * HOUR_IN_SECONDS );
-
 
 				// Set license status.
 				update_option( $premium_addon['_option_prefix'] . 'status', $license_data->license );
@@ -230,17 +242,17 @@ class WP_Travel_License {
 	 * @return void
 	 */
 	public static function deactivate_license() {
-		$count_premium_addons = WP_Travel_License::count_premium_addons();
+		$count_premium_addons = self::count_premium_addons();
 		if ( $count_premium_addons < 1 ) {
 			return;
 		}
 		foreach ( self::$addons as $key => $premium_addon ) {
 			// listen for our activate button to be clicked.
-			if ( isset( $_POST[ $premium_addon['_option_prefix'] . 'deactivate'] ) ) {
+			if ( isset( $_POST[ $premium_addon['_option_prefix'] . 'deactivate' ] ) ) {
 
 				// run a quick security check.
-			 	if ( ! check_admin_referer( $premium_addon['_option_prefix'] . 'nonce', $premium_addon['_option_prefix'] . 'nonce' ) ) {
-					 return; // get out if we didn't click the Activate button.
+				if ( ! check_admin_referer( $premium_addon['_option_prefix'] . 'nonce', $premium_addon['_option_prefix'] . 'nonce' ) ) {
+					return; // get out if we didn't click the Activate button.
 				}
 				$settings_args = get_option( 'wp_travel_settings' );
 
@@ -250,13 +262,20 @@ class WP_Travel_License {
 				// data to send in our API request.
 				$api_params = array(
 					'edd_action' => 'deactivate_license',
-					'license' 	 => $license,
+					'license'    => $license,
 					'item_name'  => urlencode( $premium_addon['item_name'] ), // the name of our product in EDD.
 					'url'        => home_url(),
 				);
 
 				// Call the custom API.
-				$response = wp_remote_post( self::$store_url, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+				$response = wp_remote_post(
+					self::$store_url,
+					array(
+						'timeout'   => 15,
+						'sslverify' => false,
+						'body'      => $api_params,
+					)
+				);
 
 				// make sure the response came back okay.
 				if ( is_wp_error( $response ) ) {
@@ -292,13 +311,20 @@ class WP_Travel_License {
 
 			$api_params = array(
 				'edd_action' => 'check_license',
-				'license' => $license,
-				'item_name' => urlencode( $addon['item_name'] ),
-				'url'       => home_url(),
+				'license'    => $license,
+				'item_name'  => urlencode( $addon['item_name'] ),
+				'url'        => home_url(),
 			);
 
 			// Call the custom API.
-			$response = wp_remote_post( self::$store_url, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+			$response = wp_remote_post(
+				self::$store_url,
+				array(
+					'timeout'   => 15,
+					'sslverify' => false,
+					'body'      => $api_params,
+				)
+			);
 
 			if ( is_wp_error( $response ) ) {
 				return false;
@@ -327,7 +353,7 @@ class WP_Travel_License {
 	 * @return Mixed
 	 */
 	public static function show_admin_notice() {
-		$count_premium_addons = WP_Travel_License::count_premium_addons();
+		$count_premium_addons = self::count_premium_addons();
 		if ( $count_premium_addons < 1 ) {
 			return;
 		}
@@ -336,9 +362,9 @@ class WP_Travel_License {
 			if ( false !== $check_license && 'valid' === $check_license ) {
 				return false;
 			}
-			$class = 'notice notice-error';
-			$link = admin_url( 'edit.php?post_type=itinerary-booking&page=settings#wp-travel-tab-content-license' );
-			$message = sprintf( __( 'You have not activated the license for %s Addon Go to <a href="%s"> settings </a> to activate your license.', 'wp-travel' ), $premium_addon['item_name'], $link );
+			$class   = 'notice notice-error';
+			$link    = admin_url( 'edit.php?post_type=itinerary-booking&page=settings#wp-travel-tab-content-license' );
+			$message = sprintf( __( 'You have not activated the license for %1$s Addon Go to <a href="%2$s"> settings </a> to activate your license.', 'wp-travel' ), $premium_addon['item_name'], $link );
 
 			printf( '<div class="%1$s"><p>%2$s</p></div>', $class, $message );
 		}
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Donate link: http://wptravel.io/downloads/
 Requires at least: 4.4.0
 Tested up to: 5.0.3
 Requires PHP: 5.5
-Stable tag: 1.8.0
+Stable tag: 1.8.1
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -185,6 +185,11 @@ WP Travel plugin has been tested with the default WordPress theme. There are add
 Before updating plugin please check ["our latest release notes"](http://wptravel.io/category/wp-travel-release/).
 
 == Changelog ==
+
+= 1.8.1 =
+Release Date: 24th January 2019
+
+* Fixes: Empty license settings tab.
 
 = 1.8.0 =
 Release Date: 23rd January 2019
