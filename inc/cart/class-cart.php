d87ed32a8dbd029dd5ba109ac85080e7b1e9357c
Rakesh <codearryaas@gmail.com>
2019-03-12 16:46:41 +0545

    fixed discount amount greater than total issue in partial

--- inc/cart/class-cart.php
+++ inc/cart/class-cart.php
@@ -456,8 +456,10 @@ class WP_Travel_Cart {
 			$d_val = $discounts['value'];
 
 			if ( 'fixed' === $d_typ ) {
-				$discount_amount = wp_travel_get_formated_price( $d_val );
-				$discount_amount_partial = wp_travel_get_formated_price( $d_val );
+				$discount_amount = $discount_amount_partial = wp_travel_get_formated_price( $d_val );
+				if ( $discount_amount_partial >= $sub_total_partial ) {
+					$discount_amount_partial = 0;
+				}
 			} elseif ( 'percentage' === $d_typ ) {
 				$discount_amount = wp_travel_get_formated_price( ( $sub_total * $d_val ) / 100 );
 				$discount_amount_partial = wp_travel_get_formated_price( ( $sub_total_partial * $d_val ) / 100 );
