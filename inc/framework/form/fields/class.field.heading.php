44731900b793c6e6419364cb6bb73dcfdf49b004
Rakesh Lawaju <codearryaas@gmail.com>
2019-03-22 12:25:19 +0545

    Heading class issue

--- inc/framework/form/fields/class.field.heading.php
+++ inc/framework/form/fields/class.field.heading.php
@@ -11,7 +11,8 @@ class WP_Travel_FW_Field_Heading {
 		$tag = isset( $this->field['heading_tag'] ) ? esc_attr( $this->field['heading_tag'] ) : 'h1';
 		$before_field = isset( $this->field['before_field'] ) ? $this->field['before_field'] : '';
 		$after_field = isset( $this->field['after_field'] ) ? $this->field['after_field'] : '';
-		$output = sprintf( '%s<%s id="%s" class="%s">%s</%s>%s', $before_field, $tag, $this->field['id'], $this->field['class'], $this->field['label'], $tag, $after_field );
+		$class = empty( $this->field['class'] ) ? $this->field['wrapper_class'] : $this->field['class'];
+		$output = sprintf( '%s<%s id="%s" class="%s">%s</%s>%s', $before_field, $tag, $this->field['id'], $class, $this->field['label'], $tag, $after_field );
 
 		if ( ! $display ) {
 			return $output;
