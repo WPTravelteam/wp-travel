f6671ebcfd09174e1202a049f7fec5e4d0a83fe8
Rakesh <codearryaas@gmail.com>
2019-04-11 16:10:46 +0545

    upsell fixes

--- inc/admin/class-admin-settings.php
+++ inc/admin/class-admin-settings.php
@@ -1131,11 +1131,14 @@ class WP_Travel_Admin_Settings {
 
 		
 		if ( ! class_exists( 'WP_Travel_Wishlists_Core' ) ) :
+			?>
+			<h3 class="wp-travel-tab-content-title"><?php echo esc_html( 'Wishlists Options', 'wp-travel' ); ?></h3>
+			<?php
 			$args = array(
 				'title'      => __( 'Allow customers to save trip for future.', 'wp-travel' ),
 				'content'    => __( 'Whishlists helps user to save trip they like for future, so that they can book them later.!', 'wp-travel' ),
-				'link'       => 'https://wptravel.io/downloads/wp-travel-field-editor/',
-				'link_label' => __( 'Get WP Travel Field Editor', 'wp-travel' ),
+				'link'       => 'https://wptravel.io/downloads/wp-travel-wishlists/',
+				'link_label' => __( 'Get WP Travel Wishlists', 'wp-travel' ),
 			);
 			wp_travel_upsell_message( $args );
 		endif;
@@ -1197,22 +1200,28 @@ class WP_Travel_Admin_Settings {
 			</tr>
 		</table>
 		<?php
-		if ( ! class_exists( 'WP_Travel_Mailchimp_Core' ) ) :
+		if ( ! class_exists( 'WP_Travel_Currency_Exchange_Rates_Core' ) ) :
+			?>
+			<h3 class="wp-travel-tab-content-title"><?php echo esc_html( 'Currency Exchange Rate API', 'wp-travel' ); ?></h3>
+			<?php
 			$args = array(
-				'title'      => __( 'Using Mailchimp for email marketing ?', 'wp-travel' ),
-				'content'    => __( 'You can import customer email from booking and inquiry to Mailchimp. That help help you grow your business.', 'wp-travel' ),
-				'link'       => 'https://wptravel.io/downloads/wp-travel-mailchimp/',
-				'link_label' => __( 'Get WP Travel Mailchimp', 'wp-travel' ),
+				'title'      => __( 'Display current exchange rate in your site.', 'wp-travel' ),
+				'content'    => __( 'You can display current exchange rate for different currency in pages or sidebar of your site.!', 'wp-travel' ),
+				'link'       => 'https://wptravel.io/downloads/',
+				'link_label' => __( 'Get WP Travel Currency Exchange Rates', 'wp-travel' ),
 			);
 			wp_travel_upsell_message( $args );
 		endif;
 		echo '<br>';
-		if ( ! class_exists( 'WP_Travel_Currency_Exchange_Rates_Core' ) ) :
+		if ( ! class_exists( 'WP_Travel_Mailchimp_Core' ) ) :
+			?>
+			<h3 class="wp-travel-tab-content-title"><?php echo esc_html( 'Mailchimp Settings', 'wp-travel' ); ?></h3>
+			<?php
 			$args = array(
-				'title'      => __( 'Display current exchange rate in your site.', 'wp-travel' ),
-				'content'    => __( 'You can display current exchange rate for different currency in pages or sidebar of your site.!', 'wp-travel' ),
-				'link'       => 'https://wptravel.io/downloads/wp-travel-field-editor/',
-				'link_label' => __( 'Get WP Travel Field Editor', 'wp-travel' ),
+				'title'      => __( 'Using Mailchimp for email marketing ?', 'wp-travel' ),
+				'content'    => __( 'You can import customer email from booking and inquiry to Mailchimp. That help you grow your business.', 'wp-travel' ),
+				'link'       => 'https://wptravel.io/downloads/wp-travel-mailchimp/',
+				'link_label' => __( 'Get WP Travel Mailchimp', 'wp-travel' ),
 			);
 			wp_travel_upsell_message( $args );
 		endif;
