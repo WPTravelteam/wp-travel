c8ba60d0c2abf8d79dd9f562140fe11759b2b726
Rakesh <codearryaas@gmail.com>
2019-04-29 14:53:24 +0545

    upsell message and links updated

--- inc/admin/class-admin-settings.php
+++ inc/admin/class-admin-settings.php
@@ -1043,7 +1043,7 @@ class WP_Travel_Admin_Settings {
 				'content'     => '',
 				'link'       => 'https://wptravel.io/wp-travel-pro/',
             	'link_label' => __( 'Get WP Travel Pro', 'wp-travel' ),
-				'link2'        => 'http://wptravel.io/downloads/',
+				'link2'        => 'https://wptravel.io/downloads/category/payment-gateways/',
 				'link2_label'  => __( 'Check All Payment Gateways', 'wp-travel' ),
 				'link3'       => 'http://wptravel.io/contact',
 				'link3_label' => __( 'Request a new one', 'wp-travel' ),
@@ -1238,7 +1238,7 @@ class WP_Travel_Admin_Settings {
 				'link2'       => 'https://wptravel.io/downloads/',
 				'link2_label' => __( 'Get WP Travel Currency Exchange Rates', 'wp-travel' ),
 			);
-			wp_travel_upsell_message( $args );
+			// wp_travel_upsell_message( $args );
 		endif;
 		echo '<br>';
 		if ( ! class_exists( 'WP_Travel_Mailchimp_Core' ) ) :
@@ -1291,7 +1291,23 @@ class WP_Travel_Admin_Settings {
 	   ?>
 	   <p><?php echo esc_html( 'You can enable/disable addons here.' ); ?></p>
 	   <?php
-	   do_action( 'wp_travel_addons_setings_tab_fields', $args );
+
+		$args = array(
+			'title' => __( 'Want to add more features in WP Travel?', 'wp-travel' ),
+			'content' => __( 'Get addon for payment, trip extras, Inventory management and other premium features.', 'wp-travel' ),
+			'link'       => 'https://wptravel.io/wp-travel-pro/',
+			'link_label' => __( 'Get WP Travel Pro', 'wp-travel' ),
+			'link2' => 'https://wptravel.io/downloads/',
+			'link2_label' => __( 'Get WP Travel Addons', 'wp-travel' ),
+		);
+
+		if ( class_exists( 'WP_Travel_Pro' ) ) {
+			$args['link'] = $args['link2'];
+			$args['link_label'] = $args['link2_label'];
+			unset( $args['link2'], $args['link2_label'] );
+		}
+		wp_travel_upsell_message( $args );
+		do_action( 'wp_travel_addons_setings_tab_fields', $args );
    }
 
 	/**
