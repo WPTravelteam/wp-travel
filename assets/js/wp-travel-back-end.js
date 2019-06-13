74c569f0065d2b7dcc1dfcd5d543590ce902c2af
Rakesh <codearryaas@gmail.com>
2019-06-13 17:29:22 +0545

    add to cart issue fixes

--- assets/js/wp-travel-back-end.js
+++ assets/js/wp-travel-back-end.js
@@ -253,7 +253,8 @@
     function wp_travel_filter_string(text) {
         return text.toString().toLowerCase()
             .replace(/\s+/g, '-') // Replace spaces with -
-            // .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+            .replace(/\,+/g, '-') // Replace spaces with -
+            // .replace(/[^\w\-]+/g, '') // Remove all non-word chars  // issue in other language if uncomment.
             .replace(/\-\-+/g, '-') // Replace multiple - with single -
             .replace(/^-+/, '') // Trim - from start of text
             .replace(/-+$/, ''); // Trim - from end of text
