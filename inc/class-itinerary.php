2c69fc74b9cbbbccb29f7769c793ffec91cf4f6d
Prakash Khadka <khadka.prakash@gmail.com>
2020-03-25 22:59:29 +0545

    gallery fix

--- inc/class-itinerary.php
+++ inc/class-itinerary.php
@@ -37,8 +37,18 @@ class WP_Travel_Itinerary {
 		$adv_gallery_ids = (array) get_post_meta( $this->post->ID, 'wp_travel_advanced_gallery', true );
 
 		if ( false !== $adv_gallery_ids && ! empty( $adv_gallery_ids ) && isset( $adv_gallery_ids['items'] ) ) {
-			return $adv_gallery_ids['items'];
-		} 
+			/**
+			 * Resolves data type issues
+			 * @since 1.0.8
+			 */
+			return array_map(
+				function( $item ) {
+					$item = (object) $item;
+					return $item->id;
+				},
+				$adv_gallery_ids['items']
+			);
+		}
 
 		return false;
 
