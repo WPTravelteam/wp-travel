715dc5a64970db9e6b1bbf32727f50f496126068
kodeninja <codearryaas@gmail.com>
2020-06-10 16:14:13 +0545

    Fixes

--- push_dot_org.sh
+++ push_dot_org.sh
@@ -28,11 +28,13 @@ echo ""
 # Copy new set of files to trunk
 echo "copy files to trunk"
 rsync -a --exclude ".git*" --exclude bash --exclude build --exclude modules --exclude inc/class-modules.php  --exclude org_assets  --exclude node_modules --exclude .editorconfig --exclude Gruntfile.js --exclude package.json --exclude package-lock.json --exclude push_dot_org.sh --exclude README.md --exclude .sass-cache --exclude inc/extended/app/src --exclude inc/extended/package.json --exclude inc/extended/postcss.config.js --exclude inc/extended/webpack.config.js --exclude inc/extended/yarn.lock --exclude inc/extended/yarn-error.log --include inc/extended/app/build     ./ ./build/trunk
+
+rsync -a ./inc/extended/app/build ./build/trunk/inc/extended/app
 echo "*********************************"
 echo ""
 
 #!/bin/bash          
-CURRENT_TAG=4.0.0
+CURRENT_TAG=4.0.1
 
 # Create tag folder
 echo "Create new tag folder"
@@ -44,6 +46,8 @@ echo ""
 # Copy new set of files to tag
 echo "copy files to tag"
 rsync -a --exclude ".git*" --exclude bash --exclude build --exclude modules --exclude inc/class-modules.php  --exclude org_assets  --exclude node_modules --exclude .editorconfig --exclude Gruntfile.js --exclude package.json --exclude package-lock.json --exclude push_dot_org.sh --exclude README.md  --exclude .sass-cache --exclude inc/extended/app/src --exclude inc/extended/package.json --exclude inc/extended/postcss.config.js --exclude inc/extended/webpack.config.js --exclude inc/extended/yarn.lock --exclude inc/extended/yarn-error.log --include inc/extended/app/build     ./ ./build/tags/$CURRENT_TAG
+
+rsync -a ./inc/extended/app/build ./build/tags/$CURRENT_TAG/inc/extended/app
 echo "*********************************"
 echo ""
 
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Donate link: https://wptravel.io/downloads/?utm_source=donate&utm_campaign=WP%20
 Requires at least: 4.4.0
 Tested up to: 5.4.1
 Requires PHP: 5.5
-Stable tag: 4.0.0
+Stable tag: 4.0.1
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -232,6 +232,12 @@ If you have any queries regarding the plugin and extensions then you can ask for
 Before updating plugin please check ["our latest release notes"](http://wptravel.io/category/wp-travel-release/).
 
 == Changelog ==
+
+= 4.0.1 =
+Release Date: 10th June, 2020
+
+Fixed: 
+Minor issues.
 
 = 4.0.0 =
 Release Date: 10th June, 2020
