7cb186ac55662dc58b04bd4326c3ccc6516acc65
Jagat K. Prajapati <prajapati.jagat@wensolutions.com>
2021-03-02 15:59:57 +0545

    fixes

--- core/ajax/cart.php
+++ core/ajax/cart.php
@@ -1,24 +1,40 @@
 <?php
+/**
+ * Ajax Request for cart.
+ *
+ * @package core/ajax
+ */
+
+/**
+ * Ajax request main class.
+ */
 class WP_Travel_Ajax_Cart {
+
+	/**
+	 * Init ajax requests.
+	 */
 	public static function init() {
-		// Get Cart items
+		// Get Cart items.
 		add_action( 'wp_ajax_wp_travel_get_cart', array( __CLASS__, 'get_cart' ) );
 		add_action( 'wp_ajax_nopriv_wp_travel_get_cart', array( __CLASS__, 'get_cart' ) );
 
-		// Add to cart
+		// Add to cart.
 		add_action( 'wp_ajax_wp_travel_add_to_cart', array( __CLASS__, 'add_to_cart' ) );
 		add_action( 'wp_ajax_nopriv_wp_travel_add_to_cart', array( __CLASS__, 'add_to_cart' ) );
 
-		// Remove item from cart
+		// Remove item from cart.
 		add_action( 'wp_ajax_wp_travel_remove_cart_item', array( __CLASS__, 'remove_cart_item' ) );
 		add_action( 'wp_ajax_nopriv_wp_travel_remove_cart_item', array( __CLASS__, 'remove_cart_item' ) );
 
-		// Remove item from cart
+		// Remove item from cart.
 		add_action( 'wp_ajax_wp_travel_update_cart_item', array( __CLASS__, 'update_cart_item' ) );
 		add_action( 'wp_ajax_nopriv_wp_travel_update_cart_item', array( __CLASS__, 'update_cart_item' ) );
 
 	}
 
+	/**
+	 * Get cart ajax request.
+	 */
 	public static function get_cart() {
 		WP_Travel::verify_nonce();
 		/**
@@ -28,6 +44,9 @@ class WP_Travel_Ajax_Cart {
 		WP_Travel_Helpers_REST_API::response( $response );
 	}
 
+	/**
+	 * Add to cart ajax request.
+	 */
 	public static function add_to_cart() {
 		WP_Travel::verify_nonce();
 		/**
@@ -41,23 +60,23 @@ class WP_Travel_Ajax_Cart {
 		WP_Travel_Helpers_REST_API::response( $response );
 	}
 
+	/**
+	 * Remove from cart ajax request.
+	 */
 	public static function remove_cart_item() {
-		WP_Travel::verify_nonce();
-		/**
-		 * We are checking nonce using WP_Travel::verify_nonce(); method.
-		 */
+		$request = WP_Travel::get_sanitize_request();
 
-		$cart_id  = ! empty( $_GET['cart_id'] ) ? absint( $_GET['cart_id'] ) : 0;
+		$cart_id  = ! empty( $request['cart_id'] ) ? $request['cart_id'] : 0;
 		$response = WP_Travel_Helpers_Cart::remove_cart_item( $cart_id );
 		WP_Travel_Helpers_REST_API::response( $response );
 	}
 
+	/**
+	 * Update cart ajax request.
+	 */
 	public static function update_cart_item() {
-		WP_Travel::verify_nonce();
-		/**
-		 * We are checking nonce using WP_Travel::verify_nonce(); method.
-		 */
-		$cart_id   = ! empty( $_GET['cart_id'] ) ? absint( $_GET['cart_id'] ) : 0;
+		$request   = WP_Travel::get_sanitize_request();
+		$cart_id   = ! empty( $request['cart_id'] ) ? absint( $request['cart_id'] ) : 0;
 		$post_data = json_decode( file_get_contents( 'php://input' ) );
 		$post_data = is_object( $post_data ) ? (array) $post_data : array();
 		$post_data = wptravel_sanitize_array( $post_data );
