ffbe95c515e641782d172669ffb95401ba753e68
Jagat K. Prajapati <prajapati.jagat@wensolutions.com>
2021-08-31 10:07:40 +0545

    individual content rendered from hook

--- app/src/admin/trip-edit/trip-options-includes-excludes.js
+++ app/src/admin/trip-edit/trip-options-includes-excludes.js
@@ -1,53 +1,59 @@
-import { useState, useEffect } from '@wordpress/element';
-import { TextControl, PanelRow, PanelBody, Button, TabPanel,Notice , FormTokenField, TextareaControl} from '@wordpress/components';
-import { applyFilters, addFilter } from '@wordpress/hooks';
-import { useSelect, dispatch } from '@wordpress/data';
-import apiFetch from '@wordpress/api-fetch';
-import { sprintf, _n, __} from '@wordpress/i18n';
+import { PanelRow } from '@wordpress/components';
+import { addFilter } from '@wordpress/hooks';
+import { dispatch } from '@wordpress/data';
+import { _n, __} from '@wordpress/i18n';
 import WPEditor from '../fields/WPEditor';
-import ErrorBoundary from '../../ErrorBoundry/ErrorBoundry';
 
 const __i18n = {
 	..._wp_travel_admin.strings
 }
-const WPTravelTripOptionsIncludesExcludesContent = () => {
-    const allData = useSelect((select) => {
-        return select('WPTravel/TripEdit').getAllStore()
-    }, []);
-    const { updateTripData, updateRequestSending, setTripData, updateStateChange } = dispatch('WPTravel/TripEdit');
-    const { trip_include, trip_exclude } = allData;
-    return <ErrorBoundary>
-        <div className="wp-travel-trip-itinerary">
-            <PanelRow>
-                <label>{__i18n.trip_includes}</label>
-            </PanelRow>
-            <PanelRow className="wp-travel-editor">
-                {'undefined' !== typeof trip_include && <WPEditor id="wp-travel-trip-includes" value={trip_include}
-                onContentChange={(trip_include) => {
-                    updateTripData({
-                        ...allData,
-                        trip_include: trip_include
-                    })
-                }} name="trip_include" />}
-            </PanelRow>
-            <PanelRow>
-                <label>{__i18n.trip_excludes}</label>
-            </PanelRow>
-            <PanelRow className="wp-travel-editor">
-                {'undefined' !== typeof trip_exclude && <WPEditor id="wp-travel-trip-excludes" value={trip_exclude}
-                onContentChange={(trip_exclude) => {
-                    updateTripData({
-                        ...allData,
-                        trip_exclude: trip_exclude
-                    })
-                }} name="trip_exclude" />}
-            </PanelRow>
-        </div>
-    </ErrorBoundary>;
-}
 
 const WPTravelTripOptionsIncludesExcludes = () => {
-    return <div className="wp-travel-ui wp-travel-ui-card wp-travel-ui-card-no-border"><WPTravelTripOptionsIncludesExcludesContent /></div>
+    return <></>;
 }
 
 export default WPTravelTripOptionsIncludesExcludes;
+
+// Single Components for hook callbacks.
+const TripIncludes = ({allData}) => {
+    const { updateTripData } = dispatch('WPTravel/TripEdit');
+    const { trip_include } = allData;
+    return <>
+        <PanelRow>
+            <label>{__i18n.trip_includes}</label>
+        </PanelRow>
+        <PanelRow className="wp-travel-editor">
+            {'undefined' !== typeof trip_include && <WPEditor id="wp-travel-trip-includes" value={trip_include}
+            onContentChange={(trip_include) => {
+                updateTripData({
+                    ...allData,
+                    trip_include: trip_include
+                })
+            }} name="trip_include" />}
+        </PanelRow>
+    </>;
+}
+
+// Single Components for hook callbacks.
+const TripExcludes = ({allData}) => {
+    const { updateTripData } = dispatch('WPTravel/TripEdit');
+    const { trip_exclude } = allData;
+    return <>
+        <PanelRow>
+            <label>{__i18n.trip_excludes}</label>
+        </PanelRow>
+        <PanelRow className="wp-travel-editor">
+            {'undefined' !== typeof trip_exclude && <WPEditor id="wp-travel-trip-excludes" value={trip_exclude}
+            onContentChange={(trip_exclude) => {
+                updateTripData({
+                    ...allData,
+                    trip_exclude: trip_exclude
+                })
+            }} name="trip_exclude" />}
+        </PanelRow>
+    </>;
+}
+
+// Hooks.
+addFilter('wptravel_trip_edit_tab_content_includes_excludes', 'wp_travel', (content, allData ) => { return [ ...content, <TripIncludes allData={allData} /> ] }, 10 );
+addFilter('wptravel_trip_edit_tab_content_includes_excludes', 'wp_travel', (content, allData ) => { return [ ...content, <TripExcludes allData={allData} /> ] }, 20 );
