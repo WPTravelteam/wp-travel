fb3427b0febcd3b456432ad3b2884878f00a867f
Jagat K. Prajapati <prajapati.jagat@wensolutions.com>
2021-09-02 13:35:50 +0545

    fetched data from hook

---
+++ app/src/admin/trip-edit/Components/Checkout/index.js
@@ -0,0 +1,33 @@
+import { Notice } from '@wordpress/components';
+import { addFilter, applyFilters } from '@wordpress/hooks';
+const __i18n = {
+	..._wp_travel_admin.strings
+}
+
+// @todo Need to remove this in future.
+const WPTravelTripOptionsLocation = () => {
+    return <>{applyFilters('wp_travel_trip_cart_checkout_tab_content', '')}</>;
+}
+export default WPTravelTripOptionsLocation;
+
+// Single Components for hook callbacks.
+const CheckoutNotice = ( {settingsData, map_data } ) => {
+    return <>
+        <Notice isDismissible={false} status="informational">
+            <strong>{__i18n.notices.checkout_option.title}</strong>
+            <br />
+            {__i18n.notices.checkout_option.description}
+            <br />
+            <br />
+            <a className="button button-primary" target="_blank" href="https://wptravel.io/wp-travel-pro/">{__i18n.notice_button_text.get_pro}</a>
+        </Notice><br />
+    </>
+}
+
+// Callbacks.
+const CheckoutNoticeCB = ( content, allData ) => {
+    return [ ...content, <CheckoutNotice allData={allData} /> ];
+}
+
+// Hooks.
+addFilter( 'wptravel_trip_edit_tab_content_cart_checkout', 'WPTravel\TripEdit\CheckoutNotice', CheckoutNoticeCB, 10 );
