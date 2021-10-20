f7a857e8519cf2a314a56af6e918a68e90b8813f
Jagat K. Prajapati <prajapati.jagat@wensolutions.com>
2021-10-20 13:35:32 +0545

    google map changes

--- app/assets/js/wp-travel-front-end-map.js
+++ app/assets/js/wp-travel-front-end-map.js
@@ -1,25 +1,47 @@
-jQuery(document).ready(function($) {
-    if ('' !== wp_travel.lat && '' !== wp_travel.lng && $( '#wp-travel-map' ).length > 0 ) {
-        // Create map.
-        var map = new GMaps({
-            div: '#wp-travel-map',
-            lat: wp_travel.lat,
-            lng: wp_travel.lng,
-            scrollwheel: false,
-            navigationControl: false,
-            mapTypeControl: false,
-            scaleControl: false,
-            // draggable: false,
-        });
+(function( $ ){
+    $.extend($.fn, {
+        wptravelGoogleMap: function( options ) {
+            if ( this.length > 0 ) {
+                // Get Selector name.
+                let mapSelector = this[0].id;
+                let selectorPrefix = '#';
+                if ( ! mapSelector ) {
+                    mapSelector = this[0].className;
+                    selectorPrefix = '.';
+                }
+                let fullSelector = selectorPrefix + mapSelector;
+                // End of getting selector name.
 
-        map.setCenter(wp_travel.lat, wp_travel.lng);
-        map.setZoom(parseInt(wp_travel.zoom));
-        map.addMarker({
-            lat: wp_travel.lat,
-            lng: wp_travel.lng,
-            title: wp_travel.loc,
-            draggable: false
+                if ( '' !== wp_travel.lat && '' !== wp_travel.lng && $( fullSelector ).length > 0 ) {
 
-        });
-    }
-});
+                    let lat  = options && options.lat ? options.lat : wp_travel.lat;
+                    let lng  = options && options.lng ? options.lng : wp_travel.lng;
+                    let zoom = options && options.zoom ? options.zoom : wp_travel.zoom;
+                    let loc  = options && options.loc ? options.loc : wp_travel.loc; // Location
+                    // Create map.
+                    var map = new GMaps({
+                        div: fullSelector,
+                        lat: lat,
+                        lng: lng,
+                        scrollwheel: false,
+                        navigationControl: false,
+                        mapTypeControl: false,
+                        scaleControl: false,
+                        // draggable: false,
+                    });
+
+                    map.setCenter(lat, lng);
+                    map.setZoom(parseInt(zoom));
+                    map.addMarker({
+                        lat: lat,
+                        lng: lng,
+                        title: loc,
+                        draggable: false
+
+                    });
+                }
+
+            }
+        }
+    });
+})( jQuery );
