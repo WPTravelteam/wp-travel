0ca77e0e5ebf30bed683ba5601f2cb3e74f65fce
Jagat K. Prajapati <prajapati.jagat@wensolutions.com>
2021-11-16 14:28:36 +0545

    field editor conflict fixes

--- inc/framework/form/fields/class.field.checkbox.php
+++ inc/framework/form/fields/class.field.checkbox.php
@@ -17,6 +17,9 @@ class WP_Travel_FW_Field_Checkbox {
 		$validations = '';
 		if ( isset( $this->field['validations'] ) ) {
 			foreach ( $this->field['validations'] as $key => $attr ) {
+				if ( 'maxlength' === $key ) {
+					continue;
+				}
 				$validations .= sprintf( 'data-parsley-%s="%s"', $key, $attr );
 			}
 		}
@@ -48,9 +51,10 @@ class WP_Travel_FW_Field_Checkbox {
 					$checked = ( $key === $this->field['default'] ) ? 'checked' : '';
 				}
 
+				$checkbox_value = is_numeric( $key ) ? $value : $key;
 				$error_coontainer_id = sprintf( 'error_container-%s', $this->field['id'] );
 				$parsley_error_container = ( 0 === $index ) ? sprintf( 'data-parsley-errors-container="#%s"', $error_coontainer_id ) : '';
-				$output .= sprintf( '<label class="radio-checkbox-label"><input type="checkbox" name="%s[]" %s value="%s" %s %s %s/>%s</label>', $this->field['name'],  $option_attributes, $key, $checked, $validations,$parsley_error_container, $value );
+				$output .= sprintf( '<label class="radio-checkbox-label"><input type="checkbox" name="%s[]" %s value="%s" %s %s %s/>%s</label>', $this->field['name'],  $option_attributes, $checkbox_value, $checked, $validations,$parsley_error_container, $value );
 				$index++;
 			}
 			$output .= sprintf( '<div id="%s"></div>', $error_coontainer_id );
