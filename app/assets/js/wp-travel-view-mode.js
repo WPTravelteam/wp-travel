1640c2858a93848a6c5e7f565c1ebd4d35d63bc9
Jagat K. Prajapati <prajapati.jagat@wensolutions.com>
2021-12-09 09:09:34 +0545

    active class added in archive toolbar button

--- app/assets/js/wp-travel-view-mode.js
+++ app/assets/js/wp-travel-view-mode.js
@@ -125,7 +125,13 @@ jQuery(document).ready(function($) {
     $(document).on( 'click', '.btn-wptravel-filter-by', function(){
         var parent = $(this).parent( '.wp-travel-filter-by-heading' );
         if ( parent &&  parent.siblings( '.wp-toolbar-filter-field' ) ) {
-            parent.siblings( '.wp-toolbar-filter-field' ).toggleClass( 'show-in-mobile' );
+            parent.siblings( '.wp-toolbar-filter-field, .wp-travel-filter-button' ).toggleClass( 'show-in-mobile' );
+
+            if ( parent.siblings( '.wp-toolbar-filter-field' ).hasClass( 'show-in-mobile' ) ) {
+                $(this).addClass( 'active' );
+            } else {
+                $(this).removeClass( 'active' );
+            }
         }
     } );
 
