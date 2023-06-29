75a2dafe2aef708bc21f68db938101379db70974
Sanchay Upadhyay <upadhyay.sanchay@wensolutions.com>
2023-06-29 14:21:38 +0545

    chage version and link of readme.txt

--- wp-travel.php
+++ wp-travel.php
@@ -3,12 +3,12 @@
  * Plugin Name: WP Travel
  * Plugin URI: http://wptravel.io/
  * Description: The best choice for a Travel Agency, Tour Operator or Destination Management Company, wanting to manage packages more efficiently & increase sales.
- * Version: 6.9.0
+ * Version: 6.9.1
  * Author: WP Travel
  * Author URI: http://wptravel.io/
  * Requires at least: 6.0.0
  * Requires PHP: 7.4
- * Tested up to: 6.2
+ * Tested up to: 6.2.2
  *
  * Text Domain: wp-travel
  * Domain Path: /i18n/languages/
@@ -38,7 +38,7 @@ if ( ! class_exists( 'WP_Travel' ) ) :
 		 *
 		 * @var string
 		 */
-		public $version = '6.9.0';
+		public $version = '6.9.1';
 
 		/**
 		 * WP Travel API version.
