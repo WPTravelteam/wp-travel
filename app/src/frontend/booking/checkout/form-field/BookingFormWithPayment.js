c5e18fdcc34f5f6463da3e1f69b89155eda57026
NSanchay <upadhyay.sanchay@gmial.com>
2023-07-05 16:37:33 +0545

    change modal component code

--- app/src/frontend/booking/checkout/form-field/BookingFormWithPayment.js
+++ app/src/frontend/booking/checkout/form-field/BookingFormWithPayment.js
@@ -16,7 +16,7 @@ export default () => {
     const bookingData  = useSelect((select) => { return select(bookingStoreName).getAllStore() }, []);
     const { updateStore } = dispatch( bookingStoreName );
     const { checkoutDetails, payment_form, form_key  } = bookingData;
-    const { booking_selected  } = checkoutDetails;
+    const { booking_selected, payment_select  } = checkoutDetails;
     const travelerData = typeof checkoutDetails != undefined && checkoutDetails[form_key] || '';
     const billingData = typeof checkoutDetails != undefined && typeof checkoutDetails.billing != 'undefined' && checkoutDetails.billing || '';
     const travelerKey = travelerData != '' && Object.keys( travelerData ) || [];
@@ -24,15 +24,8 @@ export default () => {
     const { wp_travel_booking_option } = typeof booking_selected != 'undefined' && booking_selected || "booking_with_payment"
     const { payment_gateway } = payment_form;
     const { wp_travel_payment_mode } = typeof booking_selected != 'undefined' && booking_selected || "partial"
-    // hari();
-    const ab = false;
-    const handelers = ( e ) => {
-        if ( ab == false ) {
-            return false;
-        }
-    }
     return  typeof payment_gateway != 'undefined' && <>
-    <form method="POST" action={_wp_travel.checkout_url} className="wp-travel-booking" id="wp-travel-booking" onSubmit={ ( e ) => handelers( e ) } >
+    <form method="POST" action={_wp_travel.checkout_url} className="wp-travel-booking" id="wp-travel-booking" onSubmit={ (e ) => e.preventDefault() }>
         <BookingType />
         { wp_travel_booking_option == "booking_with_payment" && <>
         <PaymentFormField />
@@ -52,6 +45,8 @@ export default () => {
            return <div key={indexs} >{ billValue != '' && <BillingHiddenField names={keyList} values={billingData[keyList]} /> }</div>
 
         } ) }
+        { wp_travel_booking_option == 'booking_with_payment' && typeof payment_select != 'undefined' && typeof payment_select.wp_travel_payment_gateway != 'undefined' && payment_select.wp_travel_payment_gateway == 'stripe' && <><label> {wp_travel.strip_card }</label><div id="card-element"></div> </>}
+        {/* <p>{stCardError.card }</p>  */}
         <div className="wp-travel-form-field button-field" >
             <input type="hidden" value={_wp_travel._nonce} name="_nonce" />
             { applyFilters( 'wptravel_booking_button_payment', [<input type="submit" name="wp_travel_book_now" id="wp-travel-book-now" value="Booke Now" /> ], bookingData ) }
