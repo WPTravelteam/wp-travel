8c091033c171ca6c92fefb5f293b937c7e564162
SanchayN <ws.sanchay@gmail.com>
2023-10-12 06:23:53 +0545

    fixed booking show on user dashboard

--- templates/account/content-dashboard.php
+++ templates/account/content-dashboard.php
@@ -13,11 +13,37 @@ $current_user         = $args['current_user'];
 $user_dashboard_menus = wptravel_sort_array_by_priority( $args['dashboard_menus'] );
 $bookings             = get_user_meta( $current_user->ID, 'wp_travel_user_bookings', true );
 $bookings_glance      = false;
+
+$getBooking = new WP_Query([
+	'post_type' => 'itinerary-booking',
+	'posts_per_page' => -1
+	
+]);
+if( empty( $bookings ) ) {
+	$bookings = [];
+}
+while($getBooking->have_posts()) {
+	$getBooking->the_post();
+	$traveler_data = get_post_meta( get_the_ID(), 'wp_travel_email_traveller', true);
+	foreach( $traveler_data as $key => $value ) {
+		if( isset($value[0] ) && $value[0] === $current_user->user_email ) {
+			if( ! empty( $bookings) ) {
+				if ( ! in_array( get_the_ID(), $bookings ) ) {
+					$bookings[] = get_the_ID();
+				}
+			} else {
+				$bookings[] = get_the_ID();
+			}
+		}
+	}
+}
+
 // Resverse Chronological Order For Bookings.
 if ( ! empty( $bookings ) && is_array( $bookings ) ) {
 	$bookings        = array_reverse( $bookings );
 	$bookings_glance = array_slice( $bookings, 0, 5 );
 }
+
 
 $biling_glance_data = get_user_meta( $current_user->ID, 'wp_travel_customer_billing_details', true );
 ?>
